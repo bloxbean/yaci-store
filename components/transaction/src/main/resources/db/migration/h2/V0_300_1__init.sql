dac0771b10802c70b10d9668380d959bd154a25f
Satya <satran004@gmail.com>
2022-11-27 20:03:20 +0800

    Sql script updated for H2 and mysql

--- components/transaction/src/main/resources/db/migration/h2/V0_300_1__init.sql
+++ components/transaction/src/main/resources/db/migration/h2/V0_300_1__init.sql
@@ -1,23 +1,24 @@
 create table transaction
 (
-    tx_hash                 varchar(255)   not null
+    tx_hash                 varchar(255) not null
         primary key,
-    auxiliary_datahash      varchar(255)   null,
-    block                   bigint         null,
-    collateral_inputs       json           null,
-    collateral_return       json           null,
-    fee                     bigint         null,
-    inputs                  json           null,
-    invalid                 bit            null,
-    network_id              int            null,
-    outputs                 json           null,
-    reference_inputs        json           null,
-    required_signers        json           null,
-    script_datahash         varchar(255)   null,
-    slot                    bigint         null,
-    total_collateral        bigint         null,
-    ttl                     bigint         null,
-    validity_interval_start bigint         null,
-    collateral_return_json  json           null
+    auxiliary_datahash      varchar(255),
+    block_hash              varchar(255),
+    block                   bigint,
+    collateral_inputs       json,
+    collateral_return       json,
+    fee                     bigint,
+    inputs                  json,
+    invalid                 boolean,
+    network_id              integer,
+    outputs                 json,
+    reference_inputs        json,
+    required_signers        json,
+    script_datahash         varchar(255),
+    slot                    bigint,
+    total_collateral        bigint,
+    ttl                     bigint,
+    validity_interval_start bigint,
+    collateral_return_json  json
 );
 
