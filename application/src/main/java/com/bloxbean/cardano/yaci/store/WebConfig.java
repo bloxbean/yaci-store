7854e68872a91570954cdf2175fc439f383b27ea
Satya <satran004@gmail.com>
2022-12-15 14:33:21 +0800

    Moved webflux to web and cors settings

---
+++ application/src/main/java/com/bloxbean/cardano/yaci/store/WebConfig.java
@@ -0,0 +1,21 @@
+package com.bloxbean.cardano.yaci.store;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true).maxAge(3600);
+    }
+
+}
