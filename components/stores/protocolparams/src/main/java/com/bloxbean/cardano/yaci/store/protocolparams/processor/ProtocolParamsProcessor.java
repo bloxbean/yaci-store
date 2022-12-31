d73486d8554022033089e0f6a05b5a037b0374a8
Satya <satran004@gmail.com>
2023-01-01 03:14:32 +0800

    Fixed null blockhash in cursor service and protocolparam service cleanup

--- components/stores/protocolparams/src/main/java/com/bloxbean/cardano/yaci/store/protocolparams/processor/ProtocolParamsProcessor.java
+++ components/stores/protocolparams/src/main/java/com/bloxbean/cardano/yaci/store/protocolparams/processor/ProtocolParamsProcessor.java
@@ -1,14 +1,11 @@
 package com.bloxbean.cardano.yaci.store.protocolparams.processor;
 
 import com.bloxbean.cardano.yaci.helper.LocalClientProvider;
-import com.bloxbean.cardano.yaci.store.events.BlockHeaderEvent;
 import com.bloxbean.cardano.yaci.store.protocolparams.service.ProtocolParamService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,22 +13,10 @@ import java.util.concurrent.TimeUnit;
 @ConditionalOnBean(LocalClientProvider.class)
 @Slf4j
 public class ProtocolParamsProcessor {
-    private long slot = 0;
     private ProtocolParamService protocolParamService;
 
     public ProtocolParamsProcessor(ProtocolParamService protocolParamService) {
         this.protocolParamService = protocolParamService;
-    }
-
-    @EventListener
-    @Transactional
-    public void handleBlockEvent(BlockHeaderEvent blockHeaderEvent) {
-        long currentSlot = blockHeaderEvent.getBlockHeader().getHeaderBody().getSlot();
-        if (currentSlot - slot < 5000) //Calculate slots in every 1000 slots
-            return;
-
-        slot = currentSlot;
-        protocolParamService.fetchAndSetCurrentProtocolParams();
     }
 
     @Scheduled(fixedRate = 5, timeUnit = TimeUnit.MINUTES)
