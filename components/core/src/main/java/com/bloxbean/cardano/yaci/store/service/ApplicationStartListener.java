972fd1cc98ebce54c1dc739f69732b019a8aaac5
Satya <satran004@gmail.com>
2023-01-01 02:42:34 +0800

    LocalClientProvider start and stop

---
+++ components/core/src/main/java/com/bloxbean/cardano/yaci/store/service/ApplicationStartListener.java
@@ -0,0 +1,37 @@
+package com.bloxbean.cardano.yaci.store.service;
+
+import com.bloxbean.cardano.yaci.helper.LocalClientProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnBean(LocalClientProvider.class)
+@RequiredArgsConstructor
+@Slf4j
+public class ApplicationStartListener {
+    private final LocalClientProvider localClientProvider;
+
+    @EventListener
+    @Async
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (localClientProvider != null && !localClientProvider.isRunning()) {
+            log.info("LocalClientProvider Started ---->>");
+            localClientProvider.start();
+        }
+    }
+
+    @EventListener
+    @Async
+    public void onApplicationEvent(ContextClosedEvent event) {
+        if (localClientProvider != null) {
+            log.info("LocalClientProvider Stopped ---->>");
+            localClientProvider.shutdown();
+        }
+    }
+}
