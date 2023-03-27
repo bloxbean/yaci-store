b5bfe10b027984ec67ac563c3ba99ef2d9aef292
Satya <satran004@gmail.com>
2023-03-27 20:39:58 +0800

    refactor(stores/metadata): Issue #5 - Added storage layer and refactored package structure

--- components/stores/metadata/src/main/java/com/bloxbean/cardano/yaci/store/metadata/service/MetadataService.java
+++ components/stores/metadata/src/main/java/com/bloxbean/cardano/yaci/store/metadata/service/MetadataService.java
@@ -1,37 +1,20 @@
 package com.bloxbean.cardano.yaci.store.metadata.service;
 
-import com.bloxbean.cardano.client.util.JsonUtil;
-import com.bloxbean.cardano.yaci.store.metadata.domain.TxMetadata;
-import com.bloxbean.cardano.yaci.store.metadata.repository.TxMetadataLabelRepository;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.bloxbean.cardano.yaci.store.metadata.domain.TxMetadataLabel;
+import com.bloxbean.cardano.yaci.store.metadata.storage.TxMetadataStorage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class MetadataService {
-    private final TxMetadataLabelRepository metadataLabelRepository;
+    private final TxMetadataStorage metadataStorage;
 
-    public List<TxMetadata> getMetadataForTx(String txHash) {
-        return metadataLabelRepository.findByTxHash(txHash)
-                .stream().map(txMetadataLabel -> {
-                    JsonNode jsonNode = null;
-
-                    try {
-                        jsonNode = JsonUtil.parseJson(txMetadataLabel.getBody());
-                    } catch (Exception e) {
-                        log.error("error parsing metadata : " + txMetadataLabel.getBody());
-                    }
-
-                    return TxMetadata.builder()
-                            .label(txMetadataLabel.getLabel())
-                            .body(jsonNode)
-                            .build();
-                }).collect(Collectors.toList());
+    public List<TxMetadataLabel> getMetadataForTx(String txHash) {
+        return metadataStorage.findByTxHash(txHash);
     }
 }
