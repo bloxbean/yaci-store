d78bdb9174c6443bd10160de5288de0496952875
Satya <satran004@gmail.com>
2023-03-30 21:57:07 +0800

    refactor(starters/blocks): Added Blocks store auto-configuration support

---
+++ starters/blocks-spring-boot-starter/src/main/java/com/bloxbean/cardano/yaci/store/starter/blocks/BlocksStoreAutoConfiguration.java
@@ -0,0 +1,18 @@
+package com.bloxbean.cardano.yaci.store.starter.blocks;
+
+import com.bloxbean.cardano.yaci.store.blocks.BlocksStoreConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+
+@AutoConfiguration
+@EnableConfigurationProperties(BlocksStoreProperties.class)
+@Import(BlocksStoreConfiguration.class)
+@Slf4j
+public class BlocksStoreAutoConfiguration {
+
+    @Autowired
+    BlocksStoreProperties properties;
+}
