8114b930c7f9f52800b2766af6d2290617b49df3
Satya <satran004@gmail.com>
2023-04-03 19:55:01 +0800

    Added gateway app and spring cloud dependency

---
+++ applications/gateway/src/main/java/com/bloxbean/cardano/yaci/store/gateway/GatewayConfig.java
@@ -0,0 +1,20 @@
+package com.bloxbean.cardano.yaci.store.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+}
