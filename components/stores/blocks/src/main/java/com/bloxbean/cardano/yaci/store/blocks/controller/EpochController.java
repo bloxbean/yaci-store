1a493e92067b8f78ecdcabb97123fed24fa7cf9c
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2023-04-04 21:27:18 +0200

    chore: add missing controller code

--- components/stores/blocks/src/main/java/com/bloxbean/cardano/yaci/store/blocks/controller/EpochController.java
+++ components/stores/blocks/src/main/java/com/bloxbean/cardano/yaci/store/blocks/controller/EpochController.java
@@ -1,8 +1,7 @@
 package com.bloxbean.cardano.yaci.store.blocks.controller;
-
-import com.bloxbean.cardano.yaci.store.blocks.domain.Block;
-import com.bloxbean.cardano.yaci.store.blocks.domain.BlocksPage;
-import com.bloxbean.cardano.yaci.store.blocks.service.BlockService;
+import com.bloxbean.cardano.yaci.store.blocks.domain.Epoch;
+import com.bloxbean.cardano.yaci.store.blocks.domain.EpochsPage;
+import com.bloxbean.cardano.yaci.store.blocks.service.EpochService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -10,32 +9,32 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("${apiPrefix}/epoch")
+@RequestMapping("${apiPrefix}/epochs")
 @Slf4j
-public class BlockController {
+public class EpochController {
 
-    private BlockService blockService;
+    private EpochService epochService;
 
-    public BlockController(BlockService blockService) {
-        this.blockService = blockService;
+    public EpochController(EpochService epochService) {
+        this.epochService = epochService;
     }
 
     @GetMapping("{number}")
-    @Operation(description = "Get block by number")
-    public Block getBlockByNumber(@PathVariable long number) {
-        return blockService.getBlockByNumber(number)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Block not found"));
+    @Operation(description = "Get epoch by number")
+    public Epoch getEpochByNumber(@PathVariable int number) {
+        return epochService.getEpochByNumber(number)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Epoch not found"));
     }
 
     @GetMapping
-    @Operation(description = "Get blocks by page number and count")
-    public BlocksPage getBlocks(@RequestParam(name = "page", defaultValue = "0") int page,
+    @Operation(description = "Get epochs by page number and count")
+    public EpochsPage getEpochs(@RequestParam(name = "page", defaultValue = "0") int page,
                                 @RequestParam(name = "count", defaultValue = "10") int count) {
         //TODO -- Fix pagination index
         int p = page;
         if (p > 0)
             p = p - 1;
-        return blockService.getBlocks(p, count);
+        return epochService.getEpochs(p, count);
     }
 
 }
