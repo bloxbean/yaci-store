0c59a6e60e93f587b8c8e925bcda528e7a26f3b5
Satya <satran004@gmail.com>
2023-04-16 21:25:24 +0800

    feat(metadata): #32 Publish biz event from metadata processor. Fix to enable custom metadata storage impl

--- components/stores/metadata/src/main/java/com/bloxbean/cardano/yaci/store/metadata/MetadataStoreConfiguration.java
+++ components/stores/metadata/src/main/java/com/bloxbean/cardano/yaci/store/metadata/MetadataStoreConfiguration.java
@@ -1,7 +1,13 @@
 package com.bloxbean.cardano.yaci.store.metadata;
 
+import com.bloxbean.cardano.yaci.store.metadata.storage.TxMetadataStorage;
+import com.bloxbean.cardano.yaci.store.metadata.storage.impl.jpa.MetadataMapper;
+import com.bloxbean.cardano.yaci.store.metadata.storage.impl.jpa.TxMetadataStorageImpl;
+import com.bloxbean.cardano.yaci.store.metadata.storage.impl.jpa.repository.TxMetadataLabelRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -19,4 +25,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EntityScan(basePackages = {"com.bloxbean.cardano.yaci.store.metadata"})
 @EnableTransactionManagement
 public class MetadataStoreConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public TxMetadataStorage txMetadataStorage(TxMetadataLabelRepository txMetadataLabelRepository,
+                                               MetadataMapper metadataMapper) {
+        return new TxMetadataStorageImpl(txMetadataLabelRepository, metadataMapper);
+    }
 }
