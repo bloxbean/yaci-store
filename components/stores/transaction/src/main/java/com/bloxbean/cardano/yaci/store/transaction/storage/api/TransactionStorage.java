a8eaaadc8fcef5e2db7c25b159ea5d27b13f191c
Satya <satran004@gmail.com>
2023-04-26 11:30:39 +0800

    refactor(transaction): Using JOOQ to query transations instead of JPA, rename storage api methods.

--- components/stores/transaction/src/main/java/com/bloxbean/cardano/yaci/store/transaction/storage/api/TransactionStorage.java
+++ components/stores/transaction/src/main/java/com/bloxbean/cardano/yaci/store/transaction/storage/api/TransactionStorage.java
@@ -1,15 +1,14 @@
 package com.bloxbean.cardano.yaci.store.transaction.storage.api;
 
+import com.bloxbean.cardano.yaci.store.common.model.Order;
 import com.bloxbean.cardano.yaci.store.transaction.domain.Txn;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TransactionStorage {
     void saveAll(List<Txn> txList);
-    Optional<Txn> findByTxHash(String txHash);
-    Page<Txn> findAll(Pageable sortedBySlot);
+    Optional<Txn> getTransactionByTxHash(String txHash);
+    List<Txn> getTransactions(int page, int count, Order order);
     int deleteBySlotGreaterThan(long slot);
 }
