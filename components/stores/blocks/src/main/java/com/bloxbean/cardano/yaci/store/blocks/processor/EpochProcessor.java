ea869387439faa9d201b05a848b24cd3de721825
Satya <satran004@gmail.com>
2023-05-06 00:36:08 +0800

    fix: #14 Fixed missing epoch calculation

--- components/stores/blocks/src/main/java/com/bloxbean/cardano/yaci/store/blocks/processor/EpochProcessor.java
+++ components/stores/blocks/src/main/java/com/bloxbean/cardano/yaci/store/blocks/processor/EpochProcessor.java
@@ -1,11 +1,16 @@
 package com.bloxbean.cardano.yaci.store.blocks.processor;
 
 import com.bloxbean.cardano.yaci.store.blocks.service.EpochService;
+import com.bloxbean.cardano.yaci.store.events.BlockHeaderEvent;
+import com.bloxbean.cardano.yaci.store.events.ByronMainBlockEvent;
+import com.bloxbean.cardano.yaci.store.events.EventMetadata;
 import com.bloxbean.cardano.yaci.store.events.RollbackEvent;
 import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,11 +23,37 @@ import java.util.concurrent.TimeUnit;
 public class EpochProcessor {
 
     private final EpochService epochService;
+    private int prevEpoch = -1;
 
     @Scheduled(fixedRate = 60, timeUnit = TimeUnit.SECONDS)
     public void scheduleEpochDataAggregation() {
         log.info("Start epoch data aggregation ....");
         epochService.aggregateData();
+    }
+
+    @EventListener
+    @Transactional
+    @Async
+    public void handleBlockHeaderEvent(@NonNull BlockHeaderEvent blockHeaderEvent) {
+        aggregateDataDuringEpochChange(blockHeaderEvent.getMetadata());
+    }
+
+    @EventListener
+    @Transactional
+    @Async
+    public void handleByronBlockHeaderEvent(ByronMainBlockEvent byronMainBlockEvent) {
+        aggregateDataDuringEpochChange(byronMainBlockEvent.getEventMetadata());
+    }
+
+    private void aggregateDataDuringEpochChange(EventMetadata eventMetadata) {
+        int currentEpoch = eventMetadata.getEpochNumber();
+        if (prevEpoch == currentEpoch)
+            return;
+
+        log.info("Epoch changed from {} to {}", prevEpoch, currentEpoch);
+        epochService.aggregateData();
+
+        prevEpoch = currentEpoch;
     }
 
     //TODO -- handle rollback event
