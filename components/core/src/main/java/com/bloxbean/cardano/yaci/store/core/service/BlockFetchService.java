9b2108621b5f336279237eaf8a609546514aa152
Satya <satran004@gmail.com>
2023-05-05 23:53:08 +0800

    fix: #14 Fixed incorrect block time

--- components/core/src/main/java/com/bloxbean/cardano/yaci/store/core/service/BlockFetchService.java
+++ components/core/src/main/java/com/bloxbean/cardano/yaci/store/core/service/BlockFetchService.java
@@ -78,13 +78,14 @@ public class BlockFetchService implements BlockChainDataListener {
         final long slot = blockHeader.getHeaderBody().getSlot();
         eraService.checkIfNewEra(era, blockHeader); //Currently it only looks for Byron to Shelley transition
         final int epochNumber = eraService.getEpochNo(era, slot);
-
+        final long blockTime = eraService.blockTime(era, slot);
 
         EventMetadata eventMetadata = EventMetadata.builder()
                 .era(era)
                 .block(blockHeader.getHeaderBody().getBlockNumber())
                 .epochNumber(epochNumber)
                 .blockHash(blockHeader.getHeaderBody().getBlockHash())
+                .blockTime(blockTime)
                 .prevBlockHash(blockHeader.getHeaderBody().getPrevHash())
                 .slot(slot)
                 .noOfTxs(transactions.size())
@@ -168,15 +169,17 @@ public class BlockFetchService implements BlockChainDataListener {
     @Override
     public void onByronBlock(ByronMainBlock byronBlock) {
         try {
-            long absoluteSlot = genesisConfig.absoluteSlot(Era.Byron,
+            final long absoluteSlot = genesisConfig.absoluteSlot(Era.Byron,
                     byronBlock.getHeader().getConsensusData().getSlotId().getEpoch(),
                     byronBlock.getHeader().getConsensusData().getSlotId().getSlot());
             final long epochNumber = byronBlock.getHeader().getConsensusData().getSlotId().getEpoch();
+            final long blockTime = eraService.blockTime(Era.Byron, absoluteSlot);
 
             EventMetadata eventMetadata = EventMetadata.builder()
                     .era(Era.Byron)
                     .block(-1)
                     .blockHash(byronBlock.getHeader().getBlockHash())
+                    .blockTime(blockTime)
                     .prevBlockHash(byronBlock.getHeader().getPrevBlock())
                     .epochNumber((int) epochNumber)
                     .slot(absoluteSlot)
@@ -205,11 +208,13 @@ public class BlockFetchService implements BlockChainDataListener {
             final long absoluteSlot = genesisConfig.absoluteSlot(Era.Byron,
                     byronEbBlock.getHeader().getConsensusData().getEpoch(), 0);
             final long epochNumber = byronEbBlock.getHeader().getConsensusData().getEpoch();
+            final long blockTime = eraService.blockTime(Era.Byron, absoluteSlot);
 
             EventMetadata eventMetadata = EventMetadata.builder()
                     .era(Era.Byron)
                     .block(-1)
                     .blockHash(byronEbBlock.getHeader().getBlockHash())
+                    .blockTime(blockTime)
                     .prevBlockHash(byronEbBlock.getHeader().getPrevBlock())
                     .epochNumber((int) epochNumber)
                     .slot(absoluteSlot)
--- components/stores/blocks/src/main/java/com/bloxbean/cardano/yaci/store/blocks/processor/ByronBlockProcessor.java
+++ components/stores/blocks/src/main/java/com/bloxbean/cardano/yaci/store/blocks/processor/ByronBlockProcessor.java
@@ -3,16 +3,12 @@ package com.bloxbean.cardano.yaci.store.blocks.processor;
 import com.bloxbean.cardano.yaci.core.model.Era;
 import com.bloxbean.cardano.yaci.core.model.byron.ByronEbBlock;
 import com.bloxbean.cardano.yaci.core.model.byron.ByronMainBlock;
-import com.bloxbean.cardano.yaci.store.blocks.configuration.BlockConfig;
 import com.bloxbean.cardano.yaci.store.blocks.domain.Block;
 import com.bloxbean.cardano.yaci.store.blocks.storage.api.BlockStorage;
-import com.bloxbean.cardano.yaci.store.blocks.util.BlockUtil;
 import com.bloxbean.cardano.yaci.store.events.ByronEbBlockEvent;
 import com.bloxbean.cardano.yaci.store.events.ByronMainBlockEvent;
 import com.bloxbean.cardano.yaci.store.events.GenesisBlockEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -24,12 +20,6 @@ import java.math.BigInteger;
 @Slf4j
 public class ByronBlockProcessor {
     private BlockStorage blockStorage;
-
-    @Autowired
-    private BlockConfig blockConfig;
-
-    @Value("${store.cardano.protocol-magic}")
-    private long protocolMagic;
 
     public ByronBlockProcessor(BlockStorage blockStorage) {
         this.blockStorage = blockStorage;
@@ -71,14 +61,9 @@ public class ByronBlockProcessor {
 
         blockStorage.findByBlockHash(byronBlock.getHeader().getPrevBlock()).ifPresent(preBlock -> {
             long blockNumber = preBlock.getNumber() + 1;
-            long time = blockConfig.getStartTime(protocolMagic);
-            long lastByronBlock = blockConfig.getLastByronBlock(protocolMagic);
-            long byronProcessingTime = blockConfig.getByronProcessTime();
-            long shellyProcessingTime = blockConfig.getShellyProcessTime();
 
             block.setNumber(blockNumber);
-            block.setBlockTime(BlockUtil.calculateBlockTime(blockNumber, slot, time,
-                    lastByronBlock, byronProcessingTime, shellyProcessingTime));
+            block.setBlockTime(event.getEventMetadata().getBlockTime());
         });
 
         blockStorage.save(block);
