0e1ba0f55252a5e25c5aa68479cc749f849e5220
Satya <satran004@gmail.com>
2023-07-22 23:31:12 +0800

    feat(metadata): Added block and blockTime to asset table

--- stores/metadata/src/main/java/com/bloxbean/cardano/yaci/store/metadata/domain/TxMetadataLabel.java
+++ stores/metadata/src/main/java/com/bloxbean/cardano/yaci/store/metadata/domain/TxMetadataLabel.java
@@ -1,18 +1,20 @@
 package com.bloxbean.cardano.yaci.store.metadata.domain;
 
+import com.bloxbean.cardano.yaci.store.common.domain.BlockAwareDomain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 @ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class TxMetadataLabel {
+public class TxMetadataLabel extends BlockAwareDomain {
     private String txHash;
     private Long slot;
     private String label;
