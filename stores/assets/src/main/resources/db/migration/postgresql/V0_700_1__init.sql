08c61ee48adf0412957444f27fcdc630a940403e
Satya <35016438+satran004@users.noreply.github.com>
2023-08-16 23:04:07 +0800

    feat(assets): Added fingerprint. Finders for unit, policy, fingerprint (#79)

--- stores/assets/src/main/resources/db/migration/postgresql/V0_700_1__init.sql
+++ stores/assets/src/main/resources/db/migration/postgresql/V0_700_1__init.sql
@@ -8,18 +8,10 @@ create table assets
     policy                varchar(255),
     asset_name            varchar(255),
     unit                  varchar(255),
+    fingerprint           varchar(255),
     quantity              bigint,
     mint_type             varchar(4),
     block                 bigint,
     block_time            bigint,
     update_datetime       timestamp
 );
-
-CREATE INDEX idx_assets_tx_hash
-    ON assets(tx_hash);
-
-CREATE INDEX idx_assets_policy
-    ON assets(policy);
-
-CREATE INDEX idx_assets_policy_assetname
-    ON assets(policy, asset_name);
