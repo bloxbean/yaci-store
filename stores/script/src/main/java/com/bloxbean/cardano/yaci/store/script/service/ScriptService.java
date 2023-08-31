a583b55682a9bce90e5a94e3ec79ee41297268fa
Satya <35016438+satran004@users.noreply.github.com>
2023-08-31 12:45:48 +0800

    feat: #82 Api to get redeemers for a transaction (#83)

--- stores/script/src/main/java/com/bloxbean/cardano/yaci/store/script/service/ScriptService.java
+++ stores/script/src/main/java/com/bloxbean/cardano/yaci/store/script/service/ScriptService.java
@@ -4,6 +4,7 @@ import com.bloxbean.cardano.client.plutus.spec.PlutusData;
 import com.bloxbean.cardano.client.plutus.spec.serializers.PlutusDataJsonConverter;
 import com.bloxbean.cardano.client.util.HexUtil;
 import com.bloxbean.cardano.yaci.store.script.domain.*;
+import com.bloxbean.cardano.yaci.store.script.dto.TxRedeemerDto;
 import com.bloxbean.cardano.yaci.store.script.helper.ScriptUtil;
 import com.bloxbean.cardano.yaci.store.script.storage.DatumStorage;
 import com.bloxbean.cardano.yaci.store.script.storage.ScriptStorage;
@@ -81,5 +82,30 @@ public class ScriptService {
                         throw new IllegalStateException("Unable to parse plutus data : " + datum.getDatum());
                     }
                 });
+    }
+
+    public List<TxRedeemerDto> getTransactionRedeemers(String txHash) {
+        List<TxScript> txScripts = txScriptStorage.findByTxHash(txHash);
+
+        if (txScripts == null || txScripts.size() == 0)
+            return Collections.EMPTY_LIST;
+
+        return txScripts.stream()
+                .filter(txScript -> txScript.getRedeemer() != null && !txScript.getRedeemer().isEmpty())
+                .map(txScript -> {
+            com.bloxbean.cardano.client.plutus.spec.Redeemer redeemer = ScriptUtil.deserializeRedeemer(txScript.getRedeemer())
+                    .orElseThrow(() -> new IllegalStateException("Unable to deserialize redeemer : " + txScript.getRedeemer()));
+            String dataHash = redeemer.getData().getDatumHash();
+
+            return TxRedeemerDto.builder()
+                    .txIndex(redeemer.getIndex().intValue())
+                    .purpose(redeemer.getTag().toString().toLowerCase())
+                    .scriptHash(txScript.getScriptHash())
+                    .redeemerDataHash(dataHash)
+                    .unitMem(String.valueOf(redeemer.getExUnits().getMem()))
+                    .unitSteps(String.valueOf(redeemer.getExUnits().getSteps()))
+                    //.fee(String.valueOf()) //TODO -- calcuate script cost from mem and steps
+                    .build();
+        }).collect(Collectors.toList());
     }
 }
