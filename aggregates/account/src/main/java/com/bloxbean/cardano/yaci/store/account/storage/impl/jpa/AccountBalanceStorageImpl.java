170dd9d06f88eaec9c76c62990ee2c0c9db9b0b8
Satya <35016438+satran004@users.noreply.github.com>
2023-10-20 19:38:01 +0800

    fix: #103 added a new method to return addreses for an asset. (#107)

--- aggregates/account/src/main/java/com/bloxbean/cardano/yaci/store/account/storage/impl/jpa/AccountBalanceStorageImpl.java
+++ aggregates/account/src/main/java/com/bloxbean/cardano/yaci/store/account/storage/impl/jpa/AccountBalanceStorageImpl.java
@@ -8,9 +8,13 @@ import com.bloxbean.cardano.yaci.store.account.storage.impl.jpa.model.AddressBal
 import com.bloxbean.cardano.yaci.store.account.storage.impl.jpa.model.StakeAddressBalanceEntity;
 import com.bloxbean.cardano.yaci.store.account.storage.impl.jpa.repository.AddressBalanceRepository;
 import com.bloxbean.cardano.yaci.store.account.storage.impl.jpa.repository.StakeBalanceRepository;
+import com.bloxbean.cardano.yaci.store.common.model.Order;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 import java.util.Optional;
@@ -101,5 +105,15 @@ public class AccountBalanceStorageImpl implements AccountBalanceStorage {
     @Override
     public int deleteStakeAddressBalanceBySlotGreaterThan(Long slot) {
         return stakeBalanceRepository.deleteBySlotGreaterThan(slot);
+    }
+
+    @Override
+    public List<AddressBalance> getAddressesByAsset(String unit, int page, int count, Order sort) {
+        Pageable sortedBySlot =
+                PageRequest.of(page, count, sort == Order.asc? Sort.by("slot").ascending() : Sort.by("slot").descending());
+
+        return addressBalanceRepository.findLatestAddressBalanceByUnit(unit, sortedBySlot).stream()
+                .map(mapper::toAddressBalance)
+                .toList();
     }
 }
