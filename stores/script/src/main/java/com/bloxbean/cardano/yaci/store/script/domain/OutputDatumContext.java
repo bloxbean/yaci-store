1f4cd3293739e3a8a9c0aaf4659c43817528c0f9
Satya <satran004@gmail.com>
2023-11-19 23:38:46 +0800

    feat: #134 Added context info for derived DatumEvent

---
+++ stores/script/src/main/java/com/bloxbean/cardano/yaci/store/script/domain/OutputDatumContext.java
@@ -0,0 +1,20 @@
+package com.bloxbean.cardano.yaci.store.script.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class OutputDatumContext {
+    private String txHash;
+    private Integer outputIndex;
+    private String outputAddress;
+    private Datum datum;
+}
