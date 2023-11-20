ea8d3590467c6ab97c1beb8e768ac88865f6a7f2
Satya <satran004@gmail.com>
2023-11-21 00:20:12 +0800

    chore: Use JOOQ to insert Datum and Scripts to avoid existsId call.

--- stores/script/src/main/java/com/bloxbean/cardano/yaci/store/script/ScriptStoreConfiguration.java
+++ stores/script/src/main/java/com/bloxbean/cardano/yaci/store/script/ScriptStoreConfiguration.java
@@ -1,11 +1,14 @@
 package com.bloxbean.cardano.yaci.store.script;
 
+import com.bloxbean.cardano.yaci.store.common.config.StoreProperties;
+import com.bloxbean.cardano.yaci.store.common.executor.ParallelExecutor;
 import com.bloxbean.cardano.yaci.store.script.storage.*;
 import com.bloxbean.cardano.yaci.store.script.storage.impl.*;
 import com.bloxbean.cardano.yaci.store.script.storage.impl.mapper.ScriptMapper;
 import com.bloxbean.cardano.yaci.store.script.storage.impl.repository.DatumRepository;
 import com.bloxbean.cardano.yaci.store.script.storage.impl.repository.ScriptRepository;
 import com.bloxbean.cardano.yaci.store.script.storage.impl.repository.TxScriptRepository;
+import org.jooq.DSLContext;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -13,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -36,14 +40,16 @@ public class ScriptStoreConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public DatumStorage datumStorage(DatumRepository datumRepository) {
-        return new DatumStorageImpl(datumRepository);
+    public DatumStorage datumStorage(DatumRepository datumRepository, DSLContext dslContext, ParallelExecutor executorHelper,
+                                     StoreProperties storeProperties, PlatformTransactionManager platformTransactionManager) {
+        return new DatumStorageImpl(datumRepository, dslContext, executorHelper, storeProperties, platformTransactionManager);
     }
 
     @Bean
     @ConditionalOnMissingBean
-    public ScriptStorage scriptStorage(ScriptRepository scriptRepository, ScriptMapper scriptMapper) {
-        return new ScriptStorageImpl(scriptRepository, scriptMapper);
+    public ScriptStorage scriptStorage(ScriptRepository scriptRepository, ScriptMapper scriptMapper, DSLContext dslContext,
+                                       ParallelExecutor executorHelper, StoreProperties storeProperties, PlatformTransactionManager platformTransactionManager) {
+        return new ScriptStorageImpl(scriptRepository, scriptMapper, dslContext, executorHelper, storeProperties, platformTransactionManager);
     }
 
     @Bean
