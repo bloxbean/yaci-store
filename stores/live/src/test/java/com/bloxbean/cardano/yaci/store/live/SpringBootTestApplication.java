54b30972a7a38ccc9b6be4fe446fc52347af7c58
Sotatek-HuyLe3a <huyle3a@sotatek.com>
2023-11-27 17:59:31 +0700

    test: add live store processor test

---
+++ stores/live/src/test/java/com/bloxbean/cardano/yaci/store/live/SpringBootTestApplication.java
@@ -0,0 +1,7 @@
+package com.bloxbean.cardano.yaci.store.live;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestApplication {
+}
