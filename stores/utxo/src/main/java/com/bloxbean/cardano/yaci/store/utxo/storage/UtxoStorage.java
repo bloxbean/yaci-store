0d9d677e8ce6c9e007f9e409c9dd511ca1117e54
Dudi Edri <dudi@anyclip.com>
2023-11-30 01:08:02 +0200

    UTxOs Storage Fixes

--- stores/utxo/src/main/java/com/bloxbean/cardano/yaci/store/utxo/storage/UtxoStorage.java
+++ stores/utxo/src/main/java/com/bloxbean/cardano/yaci/store/utxo/storage/UtxoStorage.java
@@ -8,13 +8,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UtxoStorage {
+
     void saveUnspent(List<AddressUtxo> addressUtxoList);
     void saveSpent(List<TxInput> addressUtxoList);
-
     Optional<AddressUtxo> findById(String txHash, int outputIndex);
     List<AddressUtxo> findAllByIds(List<UtxoKey> utxoKeys);
-
     int deleteUnspentBySlotGreaterThan(Long slot);
     int deleteSpentBySlotGreaterThan(Long slot);
-
+    int deleteBySpentAndBlockLessThan(Long block);
 }
