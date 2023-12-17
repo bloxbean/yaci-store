3ed3cf7b398fc7f1cd20554c104a6c60efca5b3a
Satya <satran004@gmail.com>
2023-12-17 14:54:37 +0800

    chore: Refactoring for rock types preview2

--- applications/aggregation-app/src/main/java/com/bloxbean/cardano/yaci/store/aggregation/config/EmbeddedDBConfig.java
+++ applications/aggregation-app/src/main/java/com/bloxbean/cardano/yaci/store/aggregation/config/EmbeddedDBConfig.java
@@ -2,10 +2,13 @@ package com.bloxbean.cardano.yaci.store.aggregation.config;
 
 import com.bloxbean.cardano.yaci.store.extensions.utxo.rocksdb.EmbeddedUtxoStorage;
 import com.bloxbean.cardano.yaci.store.extensions.utxo.rocksdb.EmbeddedUtxoStorageReader;
-import com.bloxbean.cardano.yaci.store.rocksdb.config.RocksDBConfig;
 import com.bloxbean.cardano.yaci.store.utxo.storage.UtxoStorage;
 import com.bloxbean.cardano.yaci.store.utxo.storage.UtxoStorageReader;
 import com.bloxbean.cardano.yaci.store.utxo.storage.impl.UtxoCache;
+import com.bloxbean.rocks.types.config.RocksDBConfig;
+import com.bloxbean.rocks.types.config.RocksDBProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,6 +21,21 @@ import org.springframework.context.annotation.Configuration;
 )
 @ComponentScan(basePackages = {"com.bloxbean.cardano.yaci.store.rocksdb"})
 public class EmbeddedDBConfig {
+
+    @Value("${store.aggr.rocksdb.base-dir:./_rocksdb}")
+    private String rocksDBBaseDir;
+
+    @Value("${store.rocksdb.column-families:}")
+    private String columnFamilies;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public RocksDBConfig rocksDBConfig() {
+        var rocksDBProperties = new RocksDBProperties();
+        rocksDBProperties.setRocksDBBaseDir(rocksDBBaseDir);
+        rocksDBProperties.setColumnFamilies(columnFamilies);
+        return new RocksDBConfig(rocksDBProperties);
+    }
 
     @Bean
     public UtxoStorage utxoStorage(RocksDBConfig rocksDBConfig, UtxoCache utxoCache) {
