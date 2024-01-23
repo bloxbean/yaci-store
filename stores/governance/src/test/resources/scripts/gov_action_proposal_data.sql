9550c28cca633ea0711ea4b3766b6ccac7ead3e5
Sotatek-HuyLe3a <huyle3a@sotatek.com>
2024-01-23 19:44:15 +0700

    chore: update governance tests

--- stores/governance/src/test/resources/scripts/gov_action_proposal_data.sql
+++ stores/governance/src/test/resources/scripts/gov_action_proposal_data.sql
@@ -1,78 +1,54 @@
 TRUNCATE TABLE gov_action_proposal;
 
 INSERT INTO gov_action_proposal
-(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, slot, block, block_time,
+(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, epoch, slot, block, block_time,
  update_datetime)
-VALUES ('2ebb61932d21ffbbbc81168d753db1c50c8849c60cefe0d7d63f4fe3c44985b2', 0, 1000000000,
-        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz', 'http://bit.ly/3QFMhii?index=0',
-        '1111111111111111111111111111111111111111111111111111111111111112', 'NEW_CONSTITUTION',
-        '{"type": "NEW_CONSTITUTION", "govActionId": null, "constitution": {"anchor": {"anchor_url": "http://bit.ly / PJt6U", "anchor_data_hash": "12fbe9d5b89dc65026a922a402ad6d94a6eb2a060ae82df4b99f63513b4813e6"}, "scripthash": null}}' FORMAT JSON,
-        14451111, 722367, 1701240092, '2024-01-05 13:18:41.074');
+VALUES ('39b3cfae7ea05e4f1ceb3bb04c599fc4a3495ecdb93f880f731eef5cf7ce3b06', 0, 1000000000,
+        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz', 'https://bit.ly/3zCH2HL',
+        '1111111111111111111111111111111111111111111111111111111111111111', 'INFO_ACTION',
+        '{"type": "INFO_ACTION"}' FORMAT JSON, 220, 19092948, 926799, 1705881929, '2024-01-23 18:16:45.423');
 INSERT INTO gov_action_proposal
-(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, slot, block, block_time,
+(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, epoch, slot, block, block_time,
  update_datetime)
-VALUES ('2ebb61932d21ffbbbc81168d753db1c50c8849c60cefe0d7d63f4fe3c44985b2', 1, 1000000000,
-        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz', 'http://bit.ly/3QFMhii?index=1',
-        '1111111111111111111111111111111111111111111111111111111111111112', 'NEW_CONSTITUTION',
-        '{"type": "NEW_CONSTITUTION", "govActionId": null, "constitution": {"anchor": {"anchor_url": "http://bit.ly / PJt6U", "anchor_data_hash": "12fbe9d5b89dc65026a922a402ad6d94a6eb2a060ae82df4b99f63513b4813e6"}, "scripthash": null}}' FORMAT JSON,
-        14451111, 722367, 1701240092, '2024-01-05 13:18:41.075');
+VALUES ('2e336f7b0e9c202fb0165827a1b836b31f0fdb4f3e273f8dd54058141e693853', 0, 1000000000,
+        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz', 'https://bit.ly/3zCH2HL',
+        '1111111111111111111111111111111111111111111111111111111111111111', 'INFO_ACTION',
+        '{"type": "INFO_ACTION"}' FORMAT JSON, 220, 19092937, 926798, 1705881918, '2024-01-23 18:16:45.411');
 INSERT INTO gov_action_proposal
-(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, slot, block, block_time,
+(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, epoch, slot, block, block_time,
  update_datetime)
-VALUES ('2ebb61932d21ffbbbc81168d753db1c50c8849c60cefe0d7d63f4fe3c44985b2', 2, 1000000000,
-        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz', 'http://bit.ly/3QFMhii?index=2',
-        '1111111111111111111111111111111111111111111111111111111111111112', 'NEW_CONSTITUTION',
-        '{"type": "NEW_CONSTITUTION", "govActionId": null, "constitution": {"anchor": {"anchor_url": "http://bit.ly / PJt6U", "anchor_data_hash": "12fbe9d5b89dc65026a922a402ad6d94a6eb2a060ae82df4b99f63513b4813e6"}, "scripthash": null}}' FORMAT JSON,
-        14451111, 722367, 1701240092, '2024-01-05 13:18:41.075');
+VALUES ('53699ec63a77450a24c74681ad45de23efa8e671dfcc4f62d663a54bb6c02cbd', 0, 1000000000,
+        'stake_test1uqvj4r4h0fh5qhvpjmwedkcnj5xysvq2plutdapw0yq69mccrcnd7', 'https://hornan7.github.io/proposal.txt',
+        '460059c9aded5a476378db48cafa45f5c0cc733b389262364207ccf5ebae1590', 'TREASURY_WITHDRAWALS_ACTION',
+        '{"type": "TREASURY_WITHDRAWALS_ACTION", "withdrawals": {"e0192a8eb77a6f405d8196dd96db13950c48300a0ff8b6f42e7901a2ef": 64000000000000}}' FORMAT JSON,
+        220, 19092418, 926776, 1705881399, '2024-01-23 18:16:45.392');
 INSERT INTO gov_action_proposal
-(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, slot, block, block_time,
+(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, epoch, slot, block, block_time,
  update_datetime)
-VALUES ('c1b514eeab070a74e882bf149a0713689e95a326a4f903ed716f89b96472601f', 0, 1000000000,
-        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz',
-        '1111111111111111111111111111111111111111111111111111111111111111',
-        '1111111111111111111111111111111111111111111111111111111111111111', 'NO_CONFIDENCE',
-        '{"type": "NO_CONFIDENCE", "govActionId": null}' FORMAT JSON, 14454644, 722552, 1701243625,
-        '2024-01-05 13:18:41.246');
+VALUES ('46e91efe1091f776318351361bf28ef7d7726b5815f5e6858f05e72f6d942d56', 0, 1000000000,
+        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz', 'https://bit.ly/3zCH2HL',
+        '1111111111111111111111111111111111111111111111111111111111111111', 'INFO_ACTION',
+        '{"type": "INFO_ACTION"}' FORMAT JSON, 219, 19006693, 923169, 1705795674, '2024-01-23 18:16:41.485');
 INSERT INTO gov_action_proposal
-(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, slot, block, block_time,
+(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, epoch, slot, block, block_time,
  update_datetime)
-VALUES ('05820007598829422f86e5f071e5f72dc36484baccf094288c5bd2a6726d78e0', 0, 1000000000,
-        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz',
-        '1111111111111111111111111111111111111111111111111111111111111111',
-        '1111111111111111111111111111111111111111111111111111111111111111', 'NO_CONFIDENCE',
-        '{"type": "NO_CONFIDENCE", "govActionId": null}' FORMAT JSON, 14454667, 722553, 1701243648,
-        '2024-01-05 13:18:41.265');
+VALUES ('cc6bc0963a68bd6e3f6b1a770e2d196a497b33e58ee5687fd8f574087164400a', 0, 1000000000,
+        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz', 'https://bit.ly/3zCH2HL',
+        '1111111111111111111111111111111111111111111111111111111111111111', 'INFO_ACTION',
+        '{"type": "INFO_ACTION"}' FORMAT JSON, 219, 19006688, 923168, 1705795669, '2024-01-23 18:16:41.474');
 INSERT INTO gov_action_proposal
-(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, slot, block, block_time,
+(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, epoch, slot, block, block_time,
  update_datetime)
-VALUES ('c37d95e2412f31d335c6c3e386655a4f8d7e76b1af1cb3c4bf2cb265dccfd5eb', 0, 1000000000,
-        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz', 'http://bit.ly/3QFMhii?index=0',
-        '1111111111111111111111111111111111111111111111111111111111111112', 'HARD_FORK_INITIATION_ACTION',
-        '{"type": "HARD_FORK_INITIATION_ACTION", "govActionId": null, "protocolVersion": {"_1": 9, "_2": 3}}' FORMAT JSON,
-        14458412, 722762, 1701247393, '2024-01-05 13:18:41.578');
+VALUES ('fc921d974f6fe0fee05e6cf5147320d5b3341ddb0fa89b2507bbc43b1b6b4e92', 1, 1000000000,
+        'stake_test1uqvj4r4h0fh5qhvpjmwedkcnj5xysvq2plutdapw0yq69mccrcnd7', 'https://hornan7.github.io/proposal.txt',
+        '460059c9aded5a476378db48cafa45f5c0cc733b389262364207ccf5ebae1590', 'TREASURY_WITHDRAWALS_ACTION',
+        '{"type": "TREASURY_WITHDRAWALS_ACTION", "withdrawals": {"e0893b50a95e7ffd6603f08cbab569f05ed73222db740ea1fb21906950": 55000000}}' FORMAT JSON,
+        219, 18961390, 921187, 1705750371, '2024-01-23 18:16:39.382');
 INSERT INTO gov_action_proposal
-(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, slot, block, block_time,
+(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, epoch, slot, block, block_time,
  update_datetime)
-VALUES ('c37d95e2412f31d335c6c3e386655a4f8d7e76b1af1cb3c4bf2cb265dccfd5eb', 1, 1000000000,
-        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz', 'http://bit.ly/3QFMhii?index=1',
-        '1111111111111111111111111111111111111111111111111111111111111112', 'HARD_FORK_INITIATION_ACTION',
-        '{"type": "HARD_FORK_INITIATION_ACTION", "govActionId": null, "protocolVersion": {"_1": 9, "_2": 3}}' FORMAT JSON,
-        14458412, 722762, 1701247393, '2024-01-05 13:18:41.578');
-INSERT INTO gov_action_proposal
-(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, slot, block, block_time,
- update_datetime)
-VALUES ('c37d95e2412f31d335c6c3e386655a4f8d7e76b1af1cb3c4bf2cb265dccfd5eb', 2, 1000000000,
-        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz', 'http://bit.ly/3QFMhii?index=2',
-        '1111111111111111111111111111111111111111111111111111111111111112', 'HARD_FORK_INITIATION_ACTION',
-        '{"type": "HARD_FORK_INITIATION_ACTION", "govActionId": null, "protocolVersion": {"_1": 9, "_2": 3}}' FORMAT JSON,
-        14458412, 722762, 1701247393, '2024-01-05 13:18:41.578');
-
-INSERT INTO gov_action_proposal
-(tx_hash, index, deposit, return_address, anchor_url, anchor_hash, type, details, slot, block, block_time,
- update_datetime)
-VALUES ('37858cdd63a31d955e42873352535d837dcbc470198da27579c0cec103414ec4', 0, 1000000000,
-        'stake_test1urd3hs7rlxwwdzthe6hj026dmyt3y0heuulctscyydh2kgck6nkmz',
-        '1111111111111111111111111111111111111111111111111111111111111111',
-        '1111111111111111111111111111111111111111111111111111111111111111', 'UPDATE_COMMITTEE',
-        '{"type": "UPDATE_COMMITTEE", "govActionId": null, "quorumThreshold": 0.5, "membersForRemoval": [], "newMembersAndTerms": {}}' FORMAT JSON,
-        14458765, 722775, 1701247746, '2024-01-05 13:18:41.574');
+VALUES ('fc921d974f6fe0fee05e6cf5147320d5b3341ddb0fa89b2507bbc43b1b6b4e92', 0, 1000000000,
+        'stake_test1uqvj4r4h0fh5qhvpjmwedkcnj5xysvq2plutdapw0yq69mccrcnd7', 'https://hornan7.github.io/proposal.txt',
+        '460059c9aded5a476378db48cafa45f5c0cc733b389262364207ccf5ebae1590', 'TREASURY_WITHDRAWALS_ACTION',
+        '{"type": "TREASURY_WITHDRAWALS_ACTION", "withdrawals": {"e02896a5eb2b697803dcf34299629ea16d9d53b8d6eea565fc7c854f35": 54000000}}' FORMAT JSON,
+        219, 18961390, 921187, 1705750371, '2024-01-23 18:16:39.382');
