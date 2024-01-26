7a766da1b564d2a161a764560fe55a9c07ad175b
Sotatek-HuyLe3a <huyle3a@sotatek.com>
2024-01-26 15:46:39 +0700

    Change primary key of voting_procedure table and update code

--- stores/governance/src/main/java/com/bloxbean/cardano/yaci/store/governance/storage/impl/repository/VotingProcedureRepository.java
+++ stores/governance/src/main/java/com/bloxbean/cardano/yaci/store/governance/storage/impl/repository/VotingProcedureRepository.java
@@ -1,16 +1,20 @@
 package com.bloxbean.cardano.yaci.store.governance.storage.impl.repository;
 
 import com.bloxbean.cardano.yaci.store.governance.storage.impl.model.VotingProcedureEntity;
+import com.bloxbean.cardano.yaci.store.governance.storage.impl.model.VotingProcedureId;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface VotingProcedureRepository extends JpaRepository<VotingProcedureEntity, UUID> {
+public interface VotingProcedureRepository extends JpaRepository<VotingProcedureEntity, VotingProcedureId> {
+    Optional<VotingProcedureEntity> findById(UUID id);
+
     int deleteBySlotGreaterThan(Long slot);
 
     List<VotingProcedureEntity> findByTxHash(String txHash);
