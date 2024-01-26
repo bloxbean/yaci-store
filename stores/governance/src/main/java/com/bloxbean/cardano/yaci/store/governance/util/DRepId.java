f85a355e6341d88c5ed24e97c6aa9a5696799a8c
Sotatek-HuyLe3a <huyle3a@sotatek.com>
2024-01-26 15:47:34 +0700

    chore: update drepId for script hash

--- stores/governance/src/main/java/com/bloxbean/cardano/yaci/store/governance/util/DRepId.java
+++ stores/governance/src/main/java/com/bloxbean/cardano/yaci/store/governance/util/DRepId.java
@@ -5,10 +5,14 @@ import com.bloxbean.cardano.client.util.HexUtil;
 
 //TODO: remove this class once cardano-client-lib is updated
 public class DRepId {
-    public static final String DREP_ID_PREFIX = "drep";
 
     public static String fromKeyHash(String keyHash) {
-        String drepId = Bech32.encode(HexUtil.decodeHexString(keyHash), DREP_ID_PREFIX);
+        String drepId = Bech32.encode(HexUtil.decodeHexString(keyHash), "drep");
+        return drepId;
+    }
+
+    public static String fromScriptHash(String scriptHash) {
+        String drepId = Bech32.encode(HexUtil.decodeHexString(scriptHash), "drep_script");
         return drepId;
     }
 }
