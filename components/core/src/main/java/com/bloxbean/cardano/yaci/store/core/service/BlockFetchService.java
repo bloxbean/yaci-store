c6c4a7e3de4a7fb670eb649e5c62f6a740d99100
Satya <satran004@gmail.com>
2024-02-08 19:50:32 +0800

    chore: Get previous epoch from cursor

--- components/core/src/main/java/com/bloxbean/cardano/yaci/store/core/service/BlockFetchService.java
+++ components/core/src/main/java/com/bloxbean/cardano/yaci/store/core/service/BlockFetchService.java
@@ -68,7 +68,7 @@ public class BlockFetchService implements BlockChainDataListener {
         final int epochSlot = eraService.getShelleyEpochSlot(slot);
         final long blockTime = eraService.blockTime(era, slot);
         final String slotLeader = SlotLeaderUtil.getShelleySlotLeader(blockHeader.getHeaderBody().getIssuerVkey());
-        final boolean newEpoch = detectIfNewEpoch(epochNumber);
+        final boolean newEpoch = detectIfNewEpoch(epochNumber, slot);
 
         //paralleMode is true when not fully synced and parallel processing is enabled
         boolean parallelMode = !syncMode && storeProperties.isEnableParallelProcessing();
@@ -140,7 +140,7 @@ public class BlockFetchService implements BlockChainDataListener {
             long blockNumber = byronBlock.getHeader().getConsensusData().getDifficulty().longValue();
             final String slotLeader = SlotLeaderUtil
                     .getByronSlotLeader(byronBlock.getHeader().getConsensusData().getPubKey());
-            final boolean newEpoch = detectIfNewEpoch((int)epochNumber);
+            final boolean newEpoch = detectIfNewEpochByronEra((int)epochNumber);
 
             //paralleMode is true when not fully synced and parallel processing is enabled
             boolean parallelMode = !syncMode && storeProperties.isEnableParallelProcessing();
@@ -381,7 +381,22 @@ public class BlockFetchService implements BlockChainDataListener {
         return (int) ((Math.random() * (max - min)) + min);
     }
 
-    private boolean detectIfNewEpoch(Integer epoch) {
+    //For shelley and later eras, this method is used to detect new epoch
+    private boolean detectIfNewEpoch(Integer epoch, long slot) {
+        if (previousEpoch == null) {
+            var prevCursor = cursorService.getPreviousCursor(slot);
+            if (prevCursor.isPresent()) {
+                previousEpoch = eraService.getEpochNo(prevCursor.get().getEra(), prevCursor.get().getSlot());
+            }
+        }
+
+        if (previousEpoch == null ||  epoch == previousEpoch + 1) {
+            return true;
+        } else
+            return false;
+    }
+
+    private boolean detectIfNewEpochByronEra(Integer epoch) {
         if (previousEpoch == null ||  epoch == previousEpoch + 1) {
             return true;
         } else
