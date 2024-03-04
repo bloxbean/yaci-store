283e331e444e967c2f493fb612357ed85ddce139
Satya <satran004@gmail.com>
2024-03-04 21:57:42 +0800

    fix: #209 Removed NUL character (\u0000) which is not allowed in Postgres jsonb column

--- stores/utxo/src/main/java/com/bloxbean/cardano/yaci/store/utxo/storage/impl/model/InvalidTransactionEntity.java
+++ stores/utxo/src/main/java/com/bloxbean/cardano/yaci/store/utxo/storage/impl/model/InvalidTransactionEntity.java
@@ -2,11 +2,10 @@ package com.bloxbean.cardano.yaci.store.utxo.storage.impl.model;
 
 import com.bloxbean.cardano.yaci.helper.model.Transaction;
 import com.bloxbean.cardano.yaci.store.common.model.BaseEntity;
+import com.bloxbean.cardano.yaci.store.common.util.JsonUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.hypersistence.utils.hibernate.type.json.JsonType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -33,4 +32,21 @@ public class InvalidTransactionEntity extends BaseEntity {
     @Type(JsonType.class)
     @Column(columnDefinition = "json")
     private Transaction transaction;
+
+    @PrePersist
+    public void prePersist() {
+        if (transaction == null) return;
+
+        var json = JsonUtil.getJson(transaction);
+        if (json.contains("\\u0000")) {
+            json = json.replace("\\u0000", "");
+
+            try {
+                var updatedTransaction = JsonUtil.getMapper().readValue(json, Transaction.class);
+                setTransaction(updatedTransaction);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("Error deserializing transaction json", e);
+            }
+        }
+    }
 }
