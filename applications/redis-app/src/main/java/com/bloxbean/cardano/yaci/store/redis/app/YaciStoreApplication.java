3b3fbade3abb6d17977fbfc61b0bf16885fdd250
Dudi Edri <dudi@anyclip.com>
2024-03-21 00:59:02 +0200

    Redis Stack support cont.

--- applications/redis-app/src/main/java/com/bloxbean/cardano/yaci/store/redis/app/YaciStoreApplication.java
+++ applications/redis-app/src/main/java/com/bloxbean/cardano/yaci/store/redis/app/YaciStoreApplication.java
@@ -10,11 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Slf4j
 @SpringBootApplication
-@ComponentScan(basePackages = "com.bloxbean.cardano.yaci.store.extensions.redis")
-@EntityScan(basePackages = "com.bloxbean.cardano.yaci.store.extensions.redis")
+@EnableRedisRepositories
+@ComponentScan(basePackages = "com.bloxbean.cardano.yaci.store")
+@EntityScan(basePackages = "com.bloxbean.cardano.yaci.store")
 public class YaciStoreApplication {
 
     public static void main(String[] args) {
--- applications/redis-app/src/main/java/com/bloxbean/cardano/yaci/store/redis/app/config/EmbeddedRedisConfiguration.java
+++ applications/redis-app/src/main/java/com/bloxbean/cardano/yaci/store/redis/app/config/EmbeddedRedisConfiguration.java
@@ -1,4 +1,4 @@
-package com.bloxbean.cardano.yaci.store.extensions.redis.blocks;
+package com.bloxbean.cardano.yaci.store.redis.app.config;
 
 import com.bloxbean.cardano.yaci.store.blocks.storage.BlockStorage;
 import com.bloxbean.cardano.yaci.store.blocks.storage.BlockStorageReader;
@@ -9,16 +9,37 @@ import com.bloxbean.cardano.yaci.store.extensions.redis.blocks.impl.RedisRollbac
 import com.bloxbean.cardano.yaci.store.extensions.redis.blocks.impl.mapper.RedisBlockMapper;
 import com.bloxbean.cardano.yaci.store.extensions.redis.blocks.impl.repository.RedisBlockRepository;
 import com.bloxbean.cardano.yaci.store.extensions.redis.blocks.impl.repository.RedisRollbackRepository;
+import com.bloxbean.cardano.yaci.store.extensions.redis.utxo.impl.RedisUtxoStorage;
+import com.bloxbean.cardano.yaci.store.extensions.redis.utxo.impl.RedisUtxoStorageReader;
+import com.bloxbean.cardano.yaci.store.extensions.redis.utxo.impl.repository.RedisTxInputRepository;
+import com.bloxbean.cardano.yaci.store.extensions.redis.utxo.impl.repository.RedisUtxoRepository;
+import com.bloxbean.cardano.yaci.store.utxo.storage.UtxoStorage;
+import com.bloxbean.cardano.yaci.store.utxo.storage.UtxoStorageReader;
+import com.bloxbean.cardano.yaci.store.utxo.storage.impl.UtxoCache;
 import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
+import com.redis.om.spring.search.stream.EntityStream;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(prefix = "store.blocks", name = "enabled", havingValue = "true", matchIfMissing = true)
-@EnableRedisDocumentRepositories(basePackages = "com.bloxbean.cardano.yaci.store.extensions.redis.blocks.*")
-public class BlocksStoreConfiguration {
+@EnableRedisDocumentRepositories(basePackages = {
+        "com.bloxbean.cardano.yaci.store.extensions.redis.utxo.*",
+        "com.bloxbean.cardano.yaci.store.extensions.redis.blocks.*"
+})
+public class EmbeddedRedisConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public UtxoStorage utxoStorage(RedisUtxoRepository utxoRepository, RedisTxInputRepository txInputRepository, UtxoCache utxoCache) {
+        return new RedisUtxoStorage(utxoRepository, txInputRepository, utxoCache);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public UtxoStorageReader utxoStorageReader(RedisUtxoRepository utxoRepository, RedisTxInputRepository txInputRepository, EntityStream entityStream) {
+        return new RedisUtxoStorageReader(utxoRepository, txInputRepository, entityStream);
+    }
 
     @Bean
     @ConditionalOnMissingBean
