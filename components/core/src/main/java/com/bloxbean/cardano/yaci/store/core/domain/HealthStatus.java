8c1bf71aa55ffaec6fdabe51acbdddffb00c5868
Satya <satran004@gmail.com>
2024-04-03 14:43:36 +0800

    chore: Added new healthstatus values

--- components/core/src/main/java/com/bloxbean/cardano/yaci/store/core/domain/HealthStatus.java
+++ components/core/src/main/java/com/bloxbean/cardano/yaci/store/core/domain/HealthStatus.java
@@ -7,6 +7,17 @@ import lombok.Data;
 @Builder
 public class HealthStatus {
     private boolean isConnectionAlive;
+
+    /**
+     * This is to indicate if the service is scheduled to stop
+     */
+    private boolean isScheduleToStop;
+
+    /**
+     * This is to indicate if there is any error
+     */
+    private boolean isError;
+
     private int lastKeepAliveResponseCookie;
     private long lastKeepAliveResponseTime;
 }
