2c098547f4fec12071c248964285640ae7da0bb8
Sotatek-QuanLeA <quan.lea@sotatek.com>
2024-04-11 16:53:11 +0700

    chore: Take snapshot balance with procedure

--- aggregates/account/src/main/java/com/bloxbean/cardano/yaci/store/account/service/BalanceSnapshotService.java
+++ aggregates/account/src/main/java/com/bloxbean/cardano/yaci/store/account/service/BalanceSnapshotService.java
@@ -1,17 +1,32 @@
 package com.bloxbean.cardano.yaci.store.account.service;
 
+import com.bloxbean.cardano.yaci.store.account.AccountStoreProperties;
 import com.bloxbean.cardano.yaci.store.core.service.CursorService;
 import com.bloxbean.cardano.yaci.store.core.service.StartService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.ParameterMode;
+import jakarta.persistence.StoredProcedureQuery;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlOutParameter;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import static com.bloxbean.cardano.yaci.store.account.job.AccountJobConstants.*;
@@ -22,12 +37,13 @@ import static com.bloxbean.cardano.yaci.store.account.job.AccountJobConstants.*;
 public class BalanceSnapshotService {
     private final CursorService cursorService;
     private final StartService startService;
-
-    private final JobLauncher jobLauncher;
-    private final Job accountBalanceJob;
+    private final JdbcTemplate jdbcTemplate;
+    private final AccountStoreProperties accountStoreProperties;
+    private final EntityManager entityManager;
 
     /**
      * Schedule balance snapshot at current cursor block
+     *
      * @return
      */
     @SneakyThrows
@@ -48,6 +64,7 @@ public class BalanceSnapshotService {
 
     /**
      * Take balance snapshot at a previous block
+     *
      * @param block
      * @param slot
      * @param blockHash
@@ -69,7 +86,7 @@ public class BalanceSnapshotService {
         }
 
         Thread.startVirtualThread(() -> {
-            takeBalanceSnapshot(block, slot, blockHash, false);
+            takeBalanceSnapshot(block, slot);
         });
 
         return true;
@@ -77,6 +94,7 @@ public class BalanceSnapshotService {
 
     /**
      * Take balance snapshot at current cursor block
+     *
      * @return
      */
     @SneakyThrows
@@ -91,23 +109,65 @@ public class BalanceSnapshotService {
             return false;
         }
 
-        takeBalanceSnapshot(cursor.getBlock(), cursor.getSlot(), cursor.getBlockHash(), true);
+        takeBalanceSnapshot(cursor.getBlock(), cursor.getSlot());
         return true;
     }
 
     @SneakyThrows
-    private void takeBalanceSnapshot(long block, long slot, String blockHash, boolean updateConfig) {
+    private void takeBalanceSnapshot(long block, long slot) {
         //Take balance snapshot
         log.info("Trying to take balance snapshot at block : {}, slot: {}", block, slot);
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addLong(SNAPSHOT_BLOCK, block)
-                .addLong(SNAPSHOT_SLOT, slot)
-                .addString(SNAPSHOT_BLOCKHASH, blockHash)
-                .addString(UPDATE_ACCOUNT_CONFIG, updateConfig? "true": "false")
-                .toJobParameters();
+        long startTime = System.currentTimeMillis();
+        int gridSize = accountStoreProperties.getBalanceCalcJobPartitionSize();
+        int batchSize = accountStoreProperties.getBalanceCalcJobBatchSize();
+        long totalAddresses = jdbcTemplate.queryForObject("SELECT max(id) FROM address", Long.class);
 
-        JobExecution jobExecution = jobLauncher.run(accountBalanceJob, jobParameters);
-        log.info("Job Execution Status: " + jobExecution.getStatus());
+
+        long partitionSize = totalAddresses / gridSize;
+
+        runParallel(partitionSize, batchSize, gridSize, totalAddresses);
+
+        log.info("Take snapshot at block : {}, slot: {}, take total time: [{} ms]", block, slot, System.currentTimeMillis() - startTime);
+
+    }
+
+    @SneakyThrows
+    private void runParallel(long partitionSize, int batchSize, int gridSize, long totalAddresses) {
+
+        List<CompletableFuture> futures = new ArrayList<>();
+        for (int i = 0; i < gridSize; i++) {
+            long startOffset = i * partitionSize;
+            long endOffset = (i == gridSize - 1) ? totalAddresses : (startOffset + partitionSize);
+            var completableFuture = CompletableFuture.supplyAsync(() -> {
+                executeStoreProcedure(startOffset, endOffset, batchSize, totalAddresses);
+                return true;
+            });
+
+            futures.add(completableFuture);
+        }
+
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        for (var future : futures) {
+            future.get();
+        }
+    }
+
+    private void executeStoreProcedure(long _from, long _to, long _batchSize, long _lastSnapshotSlot) {
+        long startTime = System.currentTimeMillis();
+        StoredProcedureQuery storedProcedure = entityManager.createStoredProcedureQuery("take_address_balance_snapshot");
+        storedProcedure.registerStoredProcedureParameter("_from", Long.class, ParameterMode.IN);
+        storedProcedure.registerStoredProcedureParameter("_to", Long.class, ParameterMode.IN);
+        storedProcedure.registerStoredProcedureParameter("_batch_size", Long.class, ParameterMode.IN);
+        storedProcedure.registerStoredProcedureParameter("_lastSnapshotSlot", Long.class, ParameterMode.IN);
+
+        storedProcedure.setParameter("_from", _from);
+        storedProcedure.setParameter("_to", _to);
+        storedProcedure.setParameter("_batch_size", _batchSize);
+        storedProcedure.setParameter("_lastSnapshotSlot", _lastSnapshotSlot);
+
+        storedProcedure.execute();
+        log.info("call address_balance [from:{} - to:{}], take[{} ms]", _from, _to, System.currentTimeMillis() - startTime);
     }
 
 }
---
+++ aggregates/account/src/main/resources/db/store/postgresql/V0_900_5__store_procedures.sql
@@ -0,0 +1,87 @@
+drop table if exists address_balance_snapshot;
+create table address_balance_snapshot
+(
+    address         character varying(500) not null,
+    unit            character varying(255) not null,
+    slot            bigint                 not null,
+    quantity        numeric(38, 0),
+    addr_full       text,
+    block           bigint,
+    block_time      bigint,
+    epoch           integer,
+    update_datetime timestamp without time zone,
+    primary key (address, unit, slot)
+);
+-- create index idx_address_balance_snapshot_address on address_balance_snapshot using btree (address);
+-- create index idx_address_balance_snapshot_block on address_balance_snapshot using btree (block);
+-- create index idx_address_balance_snapshot_block_time on address_balance_snapshot using btree (block_time);
+-- create index idx_address_balance_snapshot_epoch on address_balance_snapshot using btree (epoch);
+-- create index idx_address_balance_snapshot_slot on address_balance_snapshot using btree (slot);
+-- create index idx_address_balance_snapshot_unit on address_balance_snapshot using btree (unit);
+
+drop procedure if exists calculate_address_balance;
+CREATE OR REPLACE PROCEDURE
+    calculate_address_balance(_from numeric, _to numeric, lastSnapshotSlot NUMERIC)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+insert into address_balance_snapshot (address,
+                                      unit,
+                                      quantity,
+                                      slot,
+                                      block,
+                                      block_time,
+                                      epoch,
+                                      update_datetime)
+select address_tx_amount.address         as address,
+       address_tx_amount.unit            as unit,
+       cast(coalesce(
+               sum(address_tx_amount.quantity),
+               0
+            ) as decimal(38))            as quantity,
+       max(address_tx_amount.slot)       as slot,
+       max(address_tx_amount.block)      as block,
+       max(address_tx_amount.block_time) as block_time,
+       max(address_tx_amount.epoch)      as epoch,
+       current_timestamp
+from address_tx_amount
+where (
+          address_tx_amount.slot <= lastSnapshotSlot
+              and address_tx_amount.address in (select address.address
+                                                from address
+                                                where address.id between _from and (_to - 1)
+                                                    fetch next (_to - _from + 1) rows only)
+              )
+group by address_tx_amount.address, address_tx_amount.unit
+on conflict (address, unit, slot)
+    do update
+           set quantity   = excluded.quantity,
+           slot       = excluded.slot,
+           block      = excluded.block,
+           block_time = excluded.block_time,
+           epoch      = excluded.epoch;
+commit;
+END;
+$$;
+
+drop procedure if exists calculate_address_balance;
+CREATE OR REPLACE PROCEDURE
+    take_address_balance_snapshot(_from numeric, _to numeric, _batch_size numeric, _lastSnapshotSlot numeric)
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+current_from      numeric;
+    current_to        numeric;
+BEGIN
+    current_from := _from;
+    WHILE current_from <= _to
+        LOOP
+            current_to := current_from + _batch_size;
+call calculate_address_balance(current_from, current_to, _lastSnapshotSlot);
+commit;
+            current_from := current_to;
+END LOOP;
+END;
+$$;
