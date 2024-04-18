5a72c409c43dcbf7e7a7108da0b2d9d54c7f1bd3
Satya <satran004@gmail.com>
2024-04-18 16:24:02 +0800

    chore: Added DBIndex service to utxo-app and update create/drop index files with the new index of utxo_amount

--- applications/all/src/main/resources/sql/mysql/drop-index.sql
+++ applications/all/src/main/resources/sql/mysql/drop-index.sql
@@ -14,6 +14,7 @@ drop index idx_address_utxo_owner_paykey_hash on address_utxo;
 drop index idx_address_utxo_owner_stakekey_hash on address_utxo;
 drop index idx_address_utxo_epoch on address_utxo;
 
+drop index idx_utxo_amount_owner_addr on utxo_amount;
 drop index idx_utxo_amount_unit on utxo_amount;
 drop index idx_utxo_amount_policy on utxo_amount;
 drop index idx_utxo_amount_asset_name on utxo_amount;
