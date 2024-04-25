63edf26a282534ffc72bc0403a126ce250e5645a
Sotatek-HuyLe3a <144794833+Sotatek-HuyLe3a@users.noreply.github.com>
2024-04-25 11:22:45 +0700

    chore: update conway genesis file and config file (#261)
    * chore: update conway genesis file and config file
    
    * chore: (conway) add pvtPPSecurityGroup
    
    * chore: (conway) add minFeeRefScriptCostPerByte in param protocol
    
    * chore: (conway) update conway genesis impl
    
    * chore: (conway) minFeeRefScriptCostPerByte BigInteger -> Integer
    
    ---------
    
    Co-authored-by: Sotatek-HuyLe3a <huyle3a@sotatek.com>

--- components/common/src/test/java/com/bloxbean/carano/yaci/store/common/genesis/ConwayGenesisTest.java
+++ components/common/src/test/java/com/bloxbean/carano/yaci/store/common/genesis/ConwayGenesisTest.java
@@ -5,6 +5,8 @@ import com.bloxbean.cardano.yaci.store.common.domain.ProtocolParams;
 import com.bloxbean.cardano.yaci.store.common.genesis.ConwayGenesis;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConwayGenesisTest {
@@ -14,18 +16,11 @@ public class ConwayGenesisTest {
         ConwayGenesis conwayGenesis = new ConwayGenesis(NetworkType.MAINNET.getProtocolMagic());
         ProtocolParams protocolParams = conwayGenesis.getProtocolParams();
 
-        assertThat(protocolParams).isEqualTo(new ProtocolParams());
-    }
-
-    @Test
-    void parseConwayGenesis_sanchonet_bundled() {
-        ConwayGenesis conwayGenesis = new ConwayGenesis(NetworkType.SANCHONET.getProtocolMagic());
-        ProtocolParams protocolParams = conwayGenesis.getProtocolParams();
-
         assertThat(protocolParams.getPoolVotingThresholds().getPvtCommitteeNormal().doubleValue()).isEqualTo(0.51);
         assertThat(protocolParams.getPoolVotingThresholds().getPvtCommitteeNoConfidence().doubleValue()).isEqualTo(0.51);
         assertThat(protocolParams.getPoolVotingThresholds().getPvtHardForkInitiation().doubleValue()).isEqualTo(0.51);
         assertThat(protocolParams.getPoolVotingThresholds().getPvtMotionNoConfidence().doubleValue()).isEqualTo(0.51);
+        assertThat(protocolParams.getPoolVotingThresholds().getPvtPPSecurityGroup().doubleValue()).isEqualTo(0.51);
 
         assertThat(protocolParams.getDrepVotingThresholds().getDvtMotionNoConfidence().doubleValue()).isEqualTo(0.51);
         assertThat(protocolParams.getDrepVotingThresholds().getDvtCommitteeNormal().doubleValue()).isEqualTo(0.51);
@@ -44,5 +39,36 @@ public class ConwayGenesisTest {
         assertThat(protocolParams.getGovActionDeposit()).isEqualTo(1000000000);
         assertThat(protocolParams.getDrepDeposit()).isEqualTo(2000000);
         assertThat(protocolParams.getDrepActivity()).isEqualTo(20);
+    }
+
+    @Test
+    void parseConwayGenesis_sanchonet_bundled() {
+        ConwayGenesis conwayGenesis = new ConwayGenesis(NetworkType.SANCHONET.getProtocolMagic());
+        ProtocolParams protocolParams = conwayGenesis.getProtocolParams();
+
+        assertThat(protocolParams.getPoolVotingThresholds().getPvtCommitteeNormal().doubleValue()).isEqualTo(0.60);
+        assertThat(protocolParams.getPoolVotingThresholds().getPvtCommitteeNoConfidence().doubleValue()).isEqualTo(0.51);
+        assertThat(protocolParams.getPoolVotingThresholds().getPvtHardForkInitiation().doubleValue()).isEqualTo(0.51);
+        assertThat(protocolParams.getPoolVotingThresholds().getPvtMotionNoConfidence().doubleValue()).isEqualTo(0.60);
+        assertThat(protocolParams.getPoolVotingThresholds().getPvtPPSecurityGroup().doubleValue()).isEqualTo(0.60);
+
+        assertThat(protocolParams.getDrepVotingThresholds().getDvtMotionNoConfidence().doubleValue()).isEqualTo(0.67);
+        assertThat(protocolParams.getDrepVotingThresholds().getDvtCommitteeNormal().doubleValue()).isEqualTo(0.67);
+        assertThat(protocolParams.getDrepVotingThresholds().getDvtCommitteeNoConfidence().doubleValue()).isEqualTo(0.60);
+        assertThat(protocolParams.getDrepVotingThresholds().getDvtUpdateToConstitution().doubleValue()).isEqualTo(0.75);
+        assertThat(protocolParams.getDrepVotingThresholds().getDvtHardForkInitiation().doubleValue()).isEqualTo(0.60);
+        assertThat(protocolParams.getDrepVotingThresholds().getDvtPPNetworkGroup().doubleValue()).isEqualTo(0.67);
+        assertThat(protocolParams.getDrepVotingThresholds().getDvtPPEconomicGroup().doubleValue()).isEqualTo(0.67);
+        assertThat(protocolParams.getDrepVotingThresholds().getDvtPPTechnicalGroup().doubleValue()).isEqualTo(0.67);
+        assertThat(protocolParams.getDrepVotingThresholds().getDvtPPGovGroup().doubleValue()).isEqualTo(0.75);
+        assertThat(protocolParams.getDrepVotingThresholds().getDvtTreasuryWithdrawal().doubleValue()).isEqualTo(0.67);
+
+        assertThat(protocolParams.getCommitteeMinSize()).isEqualTo(7);
+        assertThat(protocolParams.getCommitteeMaxTermLength()).isEqualTo(73);
+        assertThat(protocolParams.getGovActionLifetime()).isEqualTo(6);
+        assertThat(protocolParams.getGovActionDeposit()).isEqualTo(BigInteger.valueOf(50000000000L));
+        assertThat(protocolParams.getDrepDeposit()).isEqualTo(500000000);
+        assertThat(protocolParams.getDrepActivity()).isEqualTo(20);
+        assertThat(protocolParams.getMinFeeRefScriptCostPerByte()).isEqualTo(44);
     }
 }
