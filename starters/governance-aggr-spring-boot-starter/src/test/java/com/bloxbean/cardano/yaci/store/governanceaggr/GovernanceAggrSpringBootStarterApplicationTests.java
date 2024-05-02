36e38b2d9c15c079f070c1a6e87da087c5917502
Sotatek-HuyLe3a <huyle3a@sotatek.com>
2024-05-02 18:26:48 +0700

    chore: (governance-aggr) fix test error

--- starters/governance-aggr-spring-boot-starter/src/test/java/com/bloxbean/cardano/yaci/store/governanceaggr/GovernanceAggrSpringBootStarterApplicationTests.java
+++ starters/governance-aggr-spring-boot-starter/src/test/java/com/bloxbean/cardano/yaci/store/governanceaggr/GovernanceAggrSpringBootStarterApplicationTests.java
@@ -1,13 +1,5 @@
 package com.bloxbean.cardano.yaci.store.governanceaggr;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class GovernanceAggrSpringBootStarterApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
