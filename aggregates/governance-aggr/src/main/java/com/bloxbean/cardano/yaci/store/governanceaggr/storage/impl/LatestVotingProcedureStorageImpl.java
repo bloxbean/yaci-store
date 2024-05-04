f5b98b9917bfd1368a95f0aedc24e0f0e48c7fb8
Sotatek-HuyLe3a <huyle3a@sotatek.com>
2024-05-04 14:13:57 +0700

    chore: (governance-aggr) CONTINUE

--- aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/LatestVotingProcedureStorageImpl.java
+++ aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/LatestVotingProcedureStorageImpl.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.bloxbean.cardano.yaci.store.governance_aggr.jooq.Tables.LATEST_VOTING_PROCEDURE;
+import static org.jooq.impl.DSL.excluded;
 
 @RequiredArgsConstructor
 public class LatestVotingProcedureStorageImpl implements LatestVotingProcedureStorage {
@@ -30,6 +31,7 @@ public class LatestVotingProcedureStorageImpl implements LatestVotingProcedureSt
         // insert on conflict do update
         var inserts = latestVotingProcedures.stream().map(latestVotingProcedure ->
                 dsl.insertInto(LATEST_VOTING_PROCEDURE)
+                        .set(LATEST_VOTING_PROCEDURE.ID, latestVotingProcedure.getId())
                         .set(LATEST_VOTING_PROCEDURE.TX_HASH, latestVotingProcedure.getTxHash())
                         .set(LATEST_VOTING_PROCEDURE.IDX, latestVotingProcedure.getIndex())
                         .set(LATEST_VOTING_PROCEDURE.SLOT, latestVotingProcedure.getSlot())
@@ -46,11 +48,14 @@ public class LatestVotingProcedureStorageImpl implements LatestVotingProcedureSt
                                 LATEST_VOTING_PROCEDURE.GOV_ACTION_TX_HASH,
                                 LATEST_VOTING_PROCEDURE.GOV_ACTION_INDEX)
                         .doUpdate()
-                        .set(LATEST_VOTING_PROCEDURE.SLOT, latestVotingProcedure.getSlot())
-                        .set(LATEST_VOTING_PROCEDURE.VOTE, latestVotingProcedure.getVote().name())
-                        .set(LATEST_VOTING_PROCEDURE.ANCHOR_URL, latestVotingProcedure.getAnchorUrl())
-                        .set(LATEST_VOTING_PROCEDURE.ANCHOR_HASH, latestVotingProcedure.getAnchorHash())
-                        .set(LATEST_VOTING_PROCEDURE.EPOCH, latestVotingProcedure.getEpoch())
+                        .set(LATEST_VOTING_PROCEDURE.IDX, excluded(LATEST_VOTING_PROCEDURE.IDX))
+                        .set(LATEST_VOTING_PROCEDURE.TX_HASH, excluded(LATEST_VOTING_PROCEDURE.TX_HASH))
+                        .set(LATEST_VOTING_PROCEDURE.VOTER_TYPE, excluded(LATEST_VOTING_PROCEDURE.VOTER_TYPE))
+                        .set(LATEST_VOTING_PROCEDURE.SLOT, excluded(LATEST_VOTING_PROCEDURE.SLOT))
+                        .set(LATEST_VOTING_PROCEDURE.VOTE, excluded(LATEST_VOTING_PROCEDURE.VOTE))
+                        .set(LATEST_VOTING_PROCEDURE.ANCHOR_URL, excluded(LATEST_VOTING_PROCEDURE.ANCHOR_URL))
+                        .set(LATEST_VOTING_PROCEDURE.ANCHOR_HASH, excluded(LATEST_VOTING_PROCEDURE.ANCHOR_HASH))
+                        .set(LATEST_VOTING_PROCEDURE.EPOCH, excluded(LATEST_VOTING_PROCEDURE.EPOCH))
                         .set(LATEST_VOTING_PROCEDURE.REPEAT_VOTE, Boolean.TRUE))
                 .collect(Collectors.toList());
 
