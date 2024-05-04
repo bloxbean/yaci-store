d8982cc4b8e3e48d7e70ad4e0a8cc6ef1525062e
Sotatek-HuyLe3a <huyle3a@sotatek.com>
2024-05-04 14:13:57 +0700

    chore: (governance-aggr) CONTINUE

--- aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/model/LatestVotingProcedureId.java
+++ aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/model/LatestVotingProcedureId.java
@@ -1,21 +1,25 @@
 package com.bloxbean.cardano.yaci.store.governanceaggr.storage.impl.model;
 
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @EqualsAndHashCode
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LatestVotingProcedureId implements Serializable {
 
-    @Column(name = "voter_hash")
+    @Column(name = "voter_hash", insertable = false, updatable = false)
     private String voterHash;
 
-    @Column(name = "gov_action_tx_hash")
+    @Column(name = "gov_action_tx_hash", insertable = false, updatable = false)
     private String govActionTxHash;
 
-    @Column(name = "gov_action_index")
+    @Column(name = "gov_action_index", insertable = false, updatable = false)
     private Integer govActionIndex;
 }
