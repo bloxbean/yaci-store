5be8780d1bd6c169ab9713783bec08d234b0703f
Sotatek-HuyLe3a <huyle3a@sotatek.com>
2024-05-06 17:58:09 +0700

    chore: fix handling commitee_vote

--- aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/CommitteeVoteStorageImpl.java
+++ aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/CommitteeVoteStorageImpl.java
@@ -1,9 +1,22 @@
 package com.bloxbean.cardano.yaci.store.governanceaggr.storage.impl;
 
+import com.bloxbean.cardano.yaci.store.governanceaggr.domain.CommitteeVote;
 import com.bloxbean.cardano.yaci.store.governanceaggr.storage.CommitteeVoteStorage;
+import com.bloxbean.cardano.yaci.store.governanceaggr.storage.impl.mapper.CommitteeVoteMapper;
+import com.bloxbean.cardano.yaci.store.governanceaggr.storage.impl.repository.CommitteeVoteRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class CommitteeVoteStorageImpl implements CommitteeVoteStorage {
+    private final CommitteeVoteRepository committeeVoteRepository;
+    private final CommitteeVoteMapper committeeVoteMapper;
 
+    @Override
+    public void saveAll(List<CommitteeVote> committeeVotes) {
+        committeeVoteRepository.saveAll(committeeVotes.stream().map(committeeVoteMapper::toCommitteeVotesEntity)
+                .collect(Collectors.toList()));
+    }
 }
