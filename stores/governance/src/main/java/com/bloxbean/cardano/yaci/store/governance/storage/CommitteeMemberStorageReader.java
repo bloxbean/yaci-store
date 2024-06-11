7d835a6d4a1bd061a5cbf288fb041ac622c4a8e4
Sotatek-HuyLe3a <huyle3a@sotatek.com>
2024-06-11 17:38:22 +0700

    fix: Avoid re-saving committee member data when restarting the service

--- stores/governance/src/main/java/com/bloxbean/cardano/yaci/store/governance/storage/CommitteeMemberStorageReader.java
+++ stores/governance/src/main/java/com/bloxbean/cardano/yaci/store/governance/storage/CommitteeMemberStorageReader.java
@@ -1,9 +1,10 @@
 package com.bloxbean.cardano.yaci.store.governance.storage;
 
+import com.bloxbean.cardano.yaci.store.common.model.Order;
 import com.bloxbean.cardano.yaci.store.governance.domain.CommitteeMember;
 
 import java.util.List;
 
 public interface CommitteeMemberStorageReader {
-    List<CommitteeMember> findCurrentMembers();
+    List<CommitteeMember> findAll(int page, int count, Order order);
 }
