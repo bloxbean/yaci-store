efc39f07318749a8673b787f87e073109428fbb1
Sotatek-HuyLe3a <huyle3a@sotatek.com>
2024-06-11 17:38:22 +0700

    fix: Avoid re-saving committee member data when restarting the service

--- stores/governance/src/main/java/com/bloxbean/cardano/yaci/store/governance/storage/impl/CommitteeMemberStorageReaderImpl.java
+++ stores/governance/src/main/java/com/bloxbean/cardano/yaci/store/governance/storage/impl/CommitteeMemberStorageReaderImpl.java
@@ -1,10 +1,14 @@
 package com.bloxbean.cardano.yaci.store.governance.storage.impl;
 
+import com.bloxbean.cardano.yaci.store.common.model.Order;
 import com.bloxbean.cardano.yaci.store.governance.domain.CommitteeMember;
 import com.bloxbean.cardano.yaci.store.governance.storage.CommitteeMemberStorageReader;
 import com.bloxbean.cardano.yaci.store.governance.storage.impl.mapper.CommitteeMemberMapper;
 import com.bloxbean.cardano.yaci.store.governance.storage.impl.repository.CommitteeMemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,10 +19,13 @@ public class CommitteeMemberStorageReaderImpl implements CommitteeMemberStorageR
     private final CommitteeMemberMapper committeeMemberMapper;
 
     @Override
-    public List<CommitteeMember> findCurrentMembers() {
-        return committeeMemberRepository.findCommitteeMemberEntitiesWithMaxSlot().stream()
+    public List<CommitteeMember> findAll(int page, int count, Order order) {
+        Pageable sortedBySlot =
+                PageRequest.of(page, count, order == Order.asc ? Sort.by("slot").ascending() : Sort.by("slot").descending());
+
+        return committeeMemberRepository.findAll(sortedBySlot)
+                .stream()
                 .map(committeeMemberMapper::toCommitteeMember)
                 .collect(Collectors.toList());
     }
-
 }
