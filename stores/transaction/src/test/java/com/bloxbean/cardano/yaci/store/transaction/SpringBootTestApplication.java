a6a25b0b09fa50b6461464c317ac62f94e1a6578
Satya <35016438+satran004@users.noreply.github.com>
2024-06-25 19:41:30 +0800

    Fix 294 : Calculate Byron Tx fee from inputs and outputs (#296)
    * fix: Calculate fee for byron txs
    
    * Cherry-pick TxEvent
    
    * Cherry-pick Test
    
    * fix: #294 Change PreCommitEvent to CommitEvent
    
    * fix: #294 Removed TxnEvent publish

--- stores/transaction/src/test/java/com/bloxbean/cardano/yaci/store/transaction/SpringBootTestApplication.java
+++ stores/transaction/src/test/java/com/bloxbean/cardano/yaci/store/transaction/SpringBootTestApplication.java
@@ -1,7 +1,15 @@
 package com.bloxbean.cardano.yaci.store.transaction;
 
+import com.bloxbean.cardano.yaci.store.client.utxo.DummyUtxoClient;
+import com.bloxbean.cardano.yaci.store.client.utxo.UtxoClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootTestApplication {
+
+    @Bean
+    public UtxoClient utxoClient() {
+        return new DummyUtxoClient();
+    }
 }
