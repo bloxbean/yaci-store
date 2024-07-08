929825f3137b4f23b28e9421bca5cd4fb73f2f6e
Satya <satran004@gmail.com>
2024-07-08 18:38:49 +0800

    chore: Add more epochs

--- aggregates/adapot/src/main/java/com/bloxbean/cardano/yaci/store/adapot/service/SharedPoolRewardAddresses.java
+++ aggregates/adapot/src/main/java/com/bloxbean/cardano/yaci/store/adapot/service/SharedPoolRewardAddresses.java
@@ -18,6 +18,25 @@ public class SharedPoolRewardAddresses {
                     "pool17rns3wjyql9jg9xkzw9h88f0kstd693pm6urwxmvejqgsyjw7ta",
                     "pool166dkk9kx5y6ug9tnvh0dnvxhwt2yca3g5pd5jaqa8t39cgyqqlr"
             );
+        } else if (epoch == 216) {
+            return Set.of(
+                    "pool13l0j202yexqh6l0awtee9g354244gmfze09utxz0sn7p7r3ev3m",
+                    "pool166dkk9kx5y6ug9tnvh0dnvxhwt2yca3g5pd5jaqa8t39cgyqqlr"
+            );
+        } else if (epoch == 218) {
+            return Set.of(
+                    "pool13l0j202yexqh6l0awtee9g354244gmfze09utxz0sn7p7r3ev3m",
+                    "pool17rns3wjyql9jg9xkzw9h88f0kstd693pm6urwxmvejqgsyjw7ta"
+            );
+        } else if (epoch == 219) {
+            return Set.of(
+                    "pool13l0j202yexqh6l0awtee9g354244gmfze09utxz0sn7p7r3ev3m",
+                    "pool166dkk9kx5y6ug9tnvh0dnvxhwt2yca3g5pd5jaqa8t39cgyqqlr"
+            );
+        } else if (epoch == 220) {
+            return Set.of(
+                    "pool13l0j202yexqh6l0awtee9g354244gmfze09utxz0sn7p7r3ev3m"
+            );
         } else
             return EMPTY_SET;
     }
