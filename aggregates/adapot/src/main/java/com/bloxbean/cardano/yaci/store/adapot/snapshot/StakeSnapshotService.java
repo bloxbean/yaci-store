0f0e34ba08ba416d12cf8cbe6e916758975dc314
Satya <satran004@gmail.com>
2024-07-08 18:39:46 +0800

    fix: Exclude pool refund happened in the same snapshot epoch

--- aggregates/adapot/src/main/java/com/bloxbean/cardano/yaci/store/adapot/snapshot/StakeSnapshotService.java
+++ aggregates/adapot/src/main/java/com/bloxbean/cardano/yaci/store/adapot/snapshot/StakeSnapshotService.java
@@ -53,9 +53,26 @@ public class StakeSnapshotService {
                               where epoch <= :epoch
                               GROUP BY address
                           ),
-                         spendable_rewards AS (
-                                  SELECT r.address, SUM(r.amount) AS withdrawable_reward
-                                  FROM reward r
+                         pool_refund_rewards AS (
+                                     SELECT r.address, SUM(r.amount) AS pool_refund_withdrawable_reward
+                                     FROM reward r
+                                              LEFT JOIN last_withdrawal lw ON r.address = lw.address
+                                     WHERE (lw.max_slot IS NULL OR r.slot > lw.max_slot)
+                                       AND r.spendable_epoch <= :epoch and r.type = 'refund'
+                                     GROUP BY r.address
+                                 ),
+                        pool_rewards AS (
+                                     SELECT r.address, SUM(r.amount) AS withdrawable_reward
+                                     FROM reward r
+                                              LEFT JOIN last_withdrawal lw ON r.address = lw.address
+                                     WHERE (lw.max_slot IS NULL OR r.slot > lw.max_slot)
+                                       AND  r.earned_epoch <= :epoch and (r.type = 'member' OR r.type = 'leader')
+                                       AND  r.spendable_epoch <= :snapshot_epoch and (r.type = 'member' OR r.type = 'leader')
+                                     GROUP BY r.address
+                         ),
+                         insta_spendable_rewards AS (
+                                  SELECT r.address, SUM(r.amount) AS insta_withdrawable_reward
+                                  FROM instant_reward r
                                            LEFT JOIN last_withdrawal lw ON r.address = lw.address
                                   WHERE (lw.max_slot IS NULL OR r.slot > lw.max_slot)
                                   AND r.spendable_epoch <= :snapshot_epoch
@@ -80,7 +97,7 @@ public class StakeSnapshotService {
                     SELECT
                         :epoch,
                         d.address,
-                        (COALESCE(s.quantity, 0) + COALESCE(r.withdrawable_reward, 0)),
+                        (COALESCE(s.quantity, 0) + COALESCE(r.withdrawable_reward, 0) + COALESCE(pr.pool_refund_withdrawable_reward, 0) + COALESCE(ir.insta_withdrawable_reward, 0)) ,
                         d.pool_id,
                         d.epoch,
                         :activeEpoch,
@@ -92,7 +109,11 @@ public class StakeSnapshotService {
                             LEFT JOIN
                         stake_address_balance s ON msb.address = s.address AND msb.max_slot = s.slot
                             LEFT JOIN
-                        spendable_rewards r ON d.address = r.address                        
+                        pool_rewards r ON d.address = r.address           
+                            LEFT JOIN
+                        pool_refund_rewards pr ON d.address = pr.address
+                            LEFT JOIN
+                        insta_spendable_rewards ir ON d.address = ir.address           
                     WHERE
                             d.rn = 1
                     and not exists(
