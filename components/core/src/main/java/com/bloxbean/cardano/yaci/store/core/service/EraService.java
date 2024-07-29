1435afd39ea538ffd236c461e179e696c04e8deb
Satya <satran004@gmail.com>
2024-07-29 15:50:17 +0800

    feat: Get current epoch from tip's slot

--- components/core/src/main/java/com/bloxbean/cardano/yaci/store/core/service/EraService.java
+++ components/core/src/main/java/com/bloxbean/cardano/yaci/store/core/service/EraService.java
@@ -12,6 +12,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
@@ -21,6 +24,7 @@ public class EraService {
     private final EpochConfig epochConfig;
     private final GenesisConfig genesisConfig;
     private final StoreProperties storeProperties;
+    private final TipFinderService tipFinderService;
 
     private Era prevEra;
     private long shelleyStartSlot = -1;
@@ -120,6 +124,22 @@ public class EraService {
         } else {
             long slotsFromShelleyStart = slot - firstShelleySlot();
             return (shelleyEraStartTime() + slotsFromShelleyStart * (long) genesisConfig.slotDuration(Era.Shelley));
+        }
+    }
+
+    /**
+     * Get current epoch number directly from the tip
+     * This method can only be used when the node is in shelley or post shelley era
+     * @return current epoch number
+     */
+    public Optional<Integer> getCurrentEpoch() {
+        var tip = tipFinderService.getTip().block(Duration.ofSeconds(5));
+
+        if (tip != null) {
+            int epoch = epochConfig.epochFromSlot(firstShelleySlot(), Era.Shelley, tip.getPoint().getSlot());
+            return Optional.of(epoch);
+        } else {
+            return Optional.empty();
         }
     }
 
