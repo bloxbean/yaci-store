cf60be172080a3352ed02e368e50166a7e81d18e
Sotatek-HuyLe3a <huyle3a@sotatek.com>
2024-09-11 15:09:23 +0700

    chore: #347 get live dRep stake distribution

--- stores/governance/src/main/java/com/bloxbean/cardano/yaci/store/governance/storage/impl/repository/LocalDRepDistrRepository.java
+++ stores/governance/src/main/java/com/bloxbean/cardano/yaci/store/governance/storage/impl/repository/LocalDRepDistrRepository.java
@@ -3,10 +3,12 @@ package com.bloxbean.cardano.yaci.store.governance.storage.impl.repository;
 import com.bloxbean.cardano.yaci.store.governance.storage.impl.model.LocalDRepDistrEntity;
 import com.bloxbean.cardano.yaci.store.governance.storage.impl.model.LocalDRepDistrId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface LocalDRepDistrRepository extends JpaRepository<LocalDRepDistrEntity, LocalDRepDistrId> {
-    Optional<LocalDRepDistrEntity> findByDrepHashAndEpoch(String dRepHash, Integer epoch);
+    Optional<LocalDRepDistrEntity> findFirstByDrepHashOrderByEpochDesc(String dRepHash);
     int deleteBySlotGreaterThan(long slot);
 }
