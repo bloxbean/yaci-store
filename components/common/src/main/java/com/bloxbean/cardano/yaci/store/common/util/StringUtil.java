ecc593977750e531e842c17027bfc29fd2b612a4
Satya <35016438+satran004@users.noreply.github.com>
2024-09-25 14:57:27 +0800

    Add policyId and assetName extraction for balances (#364)
    Refactored `AccountController` to include policyId and assetName for balance units. Implemented `getPolicyIdAndAssetName` utility method and added `isValidUTF8` method in `StringUtil` for proper asset name decoding.

--- components/common/src/main/java/com/bloxbean/cardano/yaci/store/common/util/StringUtil.java
+++ components/common/src/main/java/com/bloxbean/cardano/yaci/store/common/util/StringUtil.java
@@ -1,8 +1,24 @@
 package com.bloxbean.cardano.yaci.store.common.util;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+
 public class StringUtil {
 
     public static boolean isEmpty(String str) {
         return str == null || str.isEmpty();
+    }
+
+    public static boolean isValidUTF8(byte[] input) {
+        CharsetDecoder cs = Charset.forName("UTF-8").newDecoder();
+
+        try {
+            cs.decode(ByteBuffer.wrap(input));
+            return true;
+        } catch (CharacterCodingException e) {
+            return false;
+        }
     }
 }
