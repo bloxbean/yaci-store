5bdff4cb1e7222bf288d9a0fae7753ea17222b5b
Sotatek-HuyLe3a <huy.le3a@sotatek.com>
2024-09-30 15:17:21 +0700

    fix: fix condition

--- components/core/src/main/java/com/bloxbean/cardano/yaci/store/core/annotation/LocalDRepStake.java
+++ components/core/src/main/java/com/bloxbean/cardano/yaci/store/core/annotation/LocalDRepStake.java
@@ -7,7 +7,7 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Conditional(LocalEpochParamCondition.class)
+@Conditional(LocalDRepStakeCondition.class)
 public @interface LocalDRepStake {
 
 }
