c142856c3b25f82e93e9462f3669d3f9d5099cd9
Satya <35016438+satran004@users.noreply.github.com>
2024-10-01 19:47:01 +0800

    Feat/addr Add get transactions by address endpoint (#369)
    * Add endpoint to fetch transactions by address
    
    Introduced a new API endpoint to retrieve transactions associated with a given address. Implemented required methods in various layers including controller, service, and storage readers. This enhances functionality by allowing users to query address-specific transactions dynamically.
    
    * Update transaction summary for address API endpoint
    
    * Add API tests for address endpoints

---
+++ stores/utxo/src/main/java/com/bloxbean/cardano/yaci/store/utxo/domain/AddressTransaction.java
@@ -0,0 +1,21 @@
+package com.bloxbean.cardano.yaci.store.utxo.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class AddressTransaction {
+    private String txHash;
+    private Long blockNumber;
+    private Long blockTime;
+}
