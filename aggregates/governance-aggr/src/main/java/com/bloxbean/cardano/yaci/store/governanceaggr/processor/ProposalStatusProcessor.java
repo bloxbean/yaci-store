a341cb542d18b0f0e32a0dcf6ea4195f11b12570
Satya <satran004@gmail.com>
2024-12-03 14:21:03 +0800

    Add proposal status processing for new epoch

--- aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/processor/ProposalStatusProcessor.java
+++ aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/processor/ProposalStatusProcessor.java
@@ -2,6 +2,8 @@ package com.bloxbean.cardano.yaci.store.governanceaggr.processor;
 
 import com.bloxbean.cardano.yaci.store.events.domain.StakeSnapshotTakenEvent;
 import com.bloxbean.cardano.yaci.store.governance.storage.GovActionProposalStorage;
+import com.bloxbean.cardano.yaci.store.governance.storage.impl.model.GovActionStatus;
+import com.bloxbean.cardano.yaci.store.governanceaggr.domain.GovActionProposalStatus;
 import com.bloxbean.cardano.yaci.store.governanceaggr.service.DRepDistService;
 import com.bloxbean.cardano.yaci.store.governanceaggr.storage.GovActionProposalStatusStorage;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,21 @@ public class ProposalStatusProcessor {
         // get new active proposals in the recent epoch and save them into governance_action_proposal_status table
         // take dRep stake distribution snapshot
         // use gov rule and update proposal status
+
+        int epoch = stakeSnapshotTakenEvent.getEpoch();
+        // get new active proposals in the recent epoch and save them into governance_action_proposal_status table
+        var newProposals = govActionProposalStorage.findByEpoch(stakeSnapshotTakenEvent.getEpoch());
+
+        govActionProposalStatusStorage.saveAll(newProposals.stream().map(govActionProposal ->
+                GovActionProposalStatus.builder()
+                        .govActionTxHash(govActionProposal.getTxHash())
+                        .govActionIndex((int) govActionProposal.getIndex())
+                        .epoch(govActionProposal.getEpoch())
+                        .slot(govActionProposal.getSlot())
+                        .status(GovActionStatus.ACTIVE)
+                        .build()
+        ).toList());
+        dRepDistService.takeStakeSnapshot(stakeSnapshotTakenEvent.getEpoch());
     }
 }
 
