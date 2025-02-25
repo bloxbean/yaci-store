f7a1c31c419d5da4f95664cf7b7f5e7bf049a130
Sotatek-HuyLe3a <huy.le3a@sotatek.com>
2025-02-25 10:14:51 +0700

    chore: update class GovActionProposal in common module

--- aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/mapper/ProposalMapper.java
+++ aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/mapper/ProposalMapper.java
@@ -1,30 +1,17 @@
 package com.bloxbean.cardano.yaci.store.governanceaggr.storage.impl.mapper;
 
-import com.bloxbean.cardano.yaci.core.model.Credential;
 import com.bloxbean.cardano.yaci.core.model.governance.GovActionId;
 import com.bloxbean.cardano.yaci.core.model.governance.actions.*;
 import com.bloxbean.cardano.yaci.store.common.domain.GovActionProposal;
-import com.bloxbean.cardano.yaci.store.governance.jackson.CredentialDeserializer;
 import com.bloxbean.cardano.yaci.store.governanceaggr.domain.Proposal;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProposalMapper {
-    private final ObjectMapper objectMapper;
 
-    public ProposalMapper() {
-        this.objectMapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addKeyDeserializer(Credential.class, new CredentialDeserializer());
-        this.objectMapper.registerModule(module);
-    }
-
-    public Proposal toProposal(GovActionProposal govActionProposal) throws JsonProcessingException {
+    public Proposal toProposal(GovActionProposal govActionProposal) {
         return Proposal.builder()
-                .type(govActionProposal.getType())
+                .type(govActionProposal.getGovAction().getType())
                 .govActionId(GovActionId.builder()
                         .gov_action_index(govActionProposal.getIndex())
                         .transactionId(govActionProposal.getTxHash())
@@ -33,29 +20,16 @@ public class ProposalMapper {
                 .build();
     }
 
-    private GovActionId getPrevGovActionId(GovActionProposal govActionProposal) throws JsonProcessingException {
-        return switch (govActionProposal.getType()) {
+    private GovActionId getPrevGovActionId(GovActionProposal govActionProposal) {
+        GovAction govAction = govActionProposal.getGovAction();
+
+        return switch (govAction.getType()) {
             case TREASURY_WITHDRAWALS_ACTION, INFO_ACTION -> null;
-            case HARD_FORK_INITIATION_ACTION -> {
-                var govAction = objectMapper.treeToValue(govActionProposal.getDetails(), HardForkInitiationAction.class);
-                yield govAction.getGovActionId();
-            }
-            case NO_CONFIDENCE -> {
-                var govAction = objectMapper.treeToValue(govActionProposal.getDetails(), NoConfidence.class);
-                yield govAction.getGovActionId();
-            }
-            case UPDATE_COMMITTEE -> {
-                var govAction = objectMapper.treeToValue(govActionProposal.getDetails(), UpdateCommittee.class);
-                yield govAction.getGovActionId();
-            }
-            case NEW_CONSTITUTION -> {
-                var govAction = objectMapper.treeToValue(govActionProposal.getDetails(), NewConstitution.class);
-                yield govAction.getGovActionId();
-            }
-            case PARAMETER_CHANGE_ACTION -> {
-                var govAction = objectMapper.treeToValue(govActionProposal.getDetails(), ParameterChangeAction.class);
-                yield govAction.getGovActionId();
-            }
+            case HARD_FORK_INITIATION_ACTION -> ((HardForkInitiationAction) govAction).getGovActionId();
+            case NO_CONFIDENCE -> ((NoConfidence) govAction).getGovActionId();
+            case UPDATE_COMMITTEE -> ((UpdateCommittee) govAction).getGovActionId();
+            case NEW_CONSTITUTION -> ((NewConstitution) govAction).getGovActionId();
+            case PARAMETER_CHANGE_ACTION -> ((ParameterChangeAction) govAction).getGovActionId();
         };
     }
 
