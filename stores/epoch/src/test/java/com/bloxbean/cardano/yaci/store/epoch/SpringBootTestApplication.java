77a06151169df62c6ebbb90ba3f8de6f6739a1b2
Sotatek-HuyLe3a <huy.le3a@sotatek.com>
2025-02-25 11:01:57 +0700

    chore: #413 update test in epoch store

--- stores/epoch/src/test/java/com/bloxbean/cardano/yaci/store/epoch/SpringBootTestApplication.java
+++ stores/epoch/src/test/java/com/bloxbean/cardano/yaci/store/epoch/SpringBootTestApplication.java
@@ -1,5 +1,7 @@
 package com.bloxbean.cardano.yaci.store.epoch;
 
+import com.bloxbean.cardano.yaci.store.client.governance.DummyProposalStateClientImpl;
+import com.bloxbean.cardano.yaci.store.client.governance.ProposalStateClient;
 import com.bloxbean.cardano.yaci.store.common.config.StoreProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,5 +12,10 @@ public class SpringBootTestApplication {
     @Bean
     public StoreProperties storeProperties() {
         return new StoreProperties();
+    }
+
+    @Bean
+    public ProposalStateClient proposalStateClient() {
+        return new DummyProposalStateClientImpl();
     }
 }
