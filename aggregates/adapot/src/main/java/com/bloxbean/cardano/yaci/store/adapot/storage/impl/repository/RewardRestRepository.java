784fb070052e9252cc52ae55a38b910ff893f250
Satya <satran004@gmail.com>
2025-02-25 20:55:29 +0800

    fix: #454 Adjust treasury based on treasury withdrwals and minor refactoring.

--- aggregates/adapot/src/main/java/com/bloxbean/cardano/yaci/store/adapot/storage/impl/repository/RewardRestRepository.java
+++ aggregates/adapot/src/main/java/com/bloxbean/cardano/yaci/store/adapot/storage/impl/repository/RewardRestRepository.java
@@ -1,13 +1,17 @@
 package com.bloxbean.cardano.yaci.store.adapot.storage.impl.repository;
 
 import com.bloxbean.cardano.yaci.store.adapot.storage.impl.model.RewardRestEntity;
+import com.bloxbean.cardano.yaci.store.events.domain.RewardRestType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface RewardRestRepository extends JpaRepository<RewardRestEntity, UUID> {
 
     int deleteBySlotGreaterThan(Long slot);
+
+    List<RewardRestEntity> findBySpendableEpochAndType(Integer spendableEpoch, RewardRestType type);
 }
