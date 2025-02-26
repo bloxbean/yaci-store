3d0c6c7446e6125b9e06851351d098054fc7f93d
Sotatek-HuyLe3a <huy.le3a@sotatek.com>
2025-02-26 13:47:40 +0700

    fix: #455 fix bean missing error in governance store tests

--- stores/governance/src/test/java/com/bloxbean/cardano/yaci/store/governance/SpringBootTestApplication.java
+++ stores/governance/src/test/java/com/bloxbean/cardano/yaci/store/governance/SpringBootTestApplication.java
@@ -1,5 +1,7 @@
 package com.bloxbean.cardano.yaci.store.governance;
 
+import com.bloxbean.cardano.yaci.store.client.governance.DummyProposalStateClientImpl;
+import com.bloxbean.cardano.yaci.store.client.governance.ProposalStateClient;
 import com.bloxbean.cardano.yaci.store.common.config.StoreProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,5 +11,10 @@ public class SpringBootTestApplication {
     @Bean
     public StoreProperties storeProperties() {
         return new StoreProperties();
+    }
+
+    @Bean
+    public ProposalStateClient proposalStateClient() {
+        return new DummyProposalStateClientImpl();
     }
 }
