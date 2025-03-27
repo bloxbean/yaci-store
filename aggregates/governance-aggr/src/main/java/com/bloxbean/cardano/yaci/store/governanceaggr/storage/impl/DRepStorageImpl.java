e6362542e8a0fc54fb781b4995c2626f55746ee9
Sotatek-HuyLe3a <huy.le3a@sotatek.com>
2025-03-27 14:03:08 +0700

    chore: remove unused code

--- aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/DRepStorageImpl.java
+++ aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/DRepStorageImpl.java
@@ -80,56 +80,6 @@ public class DRepStorageImpl implements DRepStorage {
                                 .build());
     }
 
-//
-//    @Override
-//    public List<DRep> findDRepsByStatus(DRepStatus status, int page, int count, Order sort) {
-//        int offset = page * count;
-//
-//        return dslContext.selectFrom(
-//                        dslContext.select(
-//                                        DREP.DREP_ID,
-//                                        DREP.DREP_HASH,
-//                                        DREP.TX_HASH,
-//                                        DREP.CERT_INDEX,
-//                                        DREP.TX_INDEX,
-//                                        DREP.CERT_TYPE,
-//                                        DREP.STATUS,
-//                                        DREP.EPOCH,
-//                                        DREP.SLOT,
-//                                        DREP.BLOCK_HASH,
-//                                        DREP.BLOCK,
-//                                        DREP.BLOCK_TIME,
-//                                        DREP.UPDATE_DATETIME,
-//                                        rowNumber().over(partitionBy(DREP.DREP_ID)
-//                                                        .orderBy(DREP.SLOT.desc(), DREP.TX_INDEX.desc(), DREP.CERT_INDEX.desc()))
-//                                                .as("row_num")
-//                                )
-//                                .from(DREP)
-//                                .where(DREP.STATUS.eq(DRepStatus.ACTIVE.name()))
-//                                .asTable("LatestDrep")
-//                )
-//                .where(field("row_num").eq(1))
-//                .orderBy(sort == Order.desc ? field("LatestDrep.slot").desc() : field("LatestDrep.slot").asc())
-//                .limit(count)
-//                .offset(offset)
-//                .fetch()
-//                .map(record -> DRep.builder()
-//                        .drepId(record.get(DREP.DREP_ID))
-//                        .drepHash(record.get(DREP.DREP_HASH))
-//                        .txHash(record.get(DREP.TX_HASH))
-//                        .certIndex(record.get(DREP.CERT_INDEX))
-//                        .txIndex(record.get(DREP.TX_INDEX))
-//                        .certType(record.get(DREP.CERT_TYPE) != null ? CertificateType.valueOf(record.get(DREP.CERT_TYPE)) : null)
-//                        .status(DRepStatus.valueOf(record.get(DREP.STATUS)))
-//                        .epoch(record.get(DREP.EPOCH))
-//                        .slot(record.get(DREP.SLOT))
-//                        .blockHash(record.get(DREP.BLOCK_HASH))
-//                        .blockNumber(record.get(DREP.BLOCK))
-//                        .blockTime(record.get(DREP.BLOCK_TIME))
-//                        .build()
-//                );
-//    }
-
     @Override
     public int deleteBySlotGreaterThan(long slot) {
         return dRepRepository.deleteBySlotGreaterThan(slot);
