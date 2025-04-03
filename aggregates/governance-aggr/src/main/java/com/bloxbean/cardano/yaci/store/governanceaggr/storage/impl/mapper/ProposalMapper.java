863ab2ec018751cd6229d6d951b06a71e2e04be7
Sotatek-HuyLe3a <huy.le3a@sotatek.com>
2025-04-03 15:09:48 +0700

    chore: update proposal status calculation logic, refactor code, and adjust temp table in drep dist snapshot

--- aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/mapper/ProposalMapper.java
+++ aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/governanceaggr/storage/impl/mapper/ProposalMapper.java
@@ -1,13 +1,30 @@
 package com.bloxbean.cardano.yaci.store.governanceaggr.storage.impl.mapper;
 
+import com.bloxbean.cardano.yaci.core.model.Credential;
 import com.bloxbean.cardano.yaci.core.model.governance.GovActionId;
 import com.bloxbean.cardano.yaci.core.model.governance.actions.*;
 import com.bloxbean.cardano.yaci.store.common.domain.GovActionProposal;
+import com.bloxbean.cardano.yaci.store.governance.jackson.CredentialDeserializer;
 import com.bloxbean.cardano.yaci.store.governanceaggr.domain.Proposal;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
+@Slf4j
 public class ProposalMapper {
+    private final ObjectMapper objectMapper;
+
+    public ProposalMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        SimpleModule module = new SimpleModule();
+        module.addKeyDeserializer(Credential.class, new CredentialDeserializer());
+        this.objectMapper.registerModule(module);
+    }
 
     public Proposal toProposal(GovActionProposal govActionProposal) {
         return Proposal.builder()
@@ -18,6 +35,32 @@ public class ProposalMapper {
                         .build())
                 .previousGovActionId(getPrevGovActionId(govActionProposal))
                 .build();
+    }
+
+    public Optional<GovActionProposal> toGovActionProposal(com.bloxbean.cardano.yaci.store.governance.domain.GovActionProposal govActionProposal) {
+        var govActionDetailOpt = getGovAction(govActionProposal);
+
+        if (govActionDetailOpt.isEmpty()) {
+            log.error("GovAction detail missing or failed to deserialize for proposal: txHash={}, index={}",
+                    govActionProposal.getTxHash(), govActionProposal.getIndex());
+            return Optional.empty();
+        }
+
+        var govAction = govActionDetailOpt.get();
+
+        return Optional.of(GovActionProposal.builder()
+                .txHash(govActionProposal.getTxHash())
+                .index((int) govActionProposal.getIndex())
+                .anchorUrl(govActionProposal.getAnchorUrl())
+                .anchorHash(govActionProposal.getAnchorHash())
+                .deposit(govActionProposal.getDeposit())
+                .returnAddress(govActionProposal.getReturnAddress())
+                .blockNumber(govActionProposal.getBlockNumber())
+                .blockTime(govActionProposal.getBlockTime())
+                .slot(govActionProposal.getSlot())
+                .epoch(govActionProposal.getEpoch())
+                .govAction(govAction)
+                .build());
     }
 
     private GovActionId getPrevGovActionId(GovActionProposal govActionProposal) {
@@ -33,4 +76,30 @@ public class ProposalMapper {
         };
     }
 
+    private Optional<GovAction> getGovAction(com.bloxbean.cardano.yaci.store.governance.domain.GovActionProposal govActionProposal) {
+        GovAction govAction = null;
+
+        try {
+            switch (govActionProposal.getType()) {
+                case INFO_ACTION -> govAction = new InfoAction();
+                case HARD_FORK_INITIATION_ACTION ->
+                        govAction = objectMapper.treeToValue(govActionProposal.getDetails(), HardForkInitiationAction.class);
+                case TREASURY_WITHDRAWALS_ACTION ->
+                        govAction = objectMapper.treeToValue(govActionProposal.getDetails(), TreasuryWithdrawalsAction.class);
+                case NO_CONFIDENCE ->
+                        govAction = objectMapper.treeToValue(govActionProposal.getDetails(), NoConfidence.class);
+                case UPDATE_COMMITTEE ->
+                        govAction = objectMapper.treeToValue(govActionProposal.getDetails(), UpdateCommittee.class);
+                case NEW_CONSTITUTION ->
+                        govAction = objectMapper.treeToValue(govActionProposal.getDetails(), NewConstitution.class);
+                case PARAMETER_CHANGE_ACTION ->
+                        govAction = objectMapper.treeToValue(govActionProposal.getDetails(), ParameterChangeAction.class);
+            }
+        } catch (JsonProcessingException e) {
+            log.error("Error processing JSON for gov action detail, GovActionID: tx hash {} , index {}",
+                    govActionProposal.getTxHash(), govActionProposal.getIndex(), e);
+        }
+
+        return Optional.ofNullable(govAction);
+    }
 }
