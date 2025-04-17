7062426314292782f5532988322aaae90835d7c8
Satya <satran004@gmail.com>
2025-04-17 20:38:03 +0800

    chore: Fix docker build for admin-cli, tag in .env

--- docker/admin-cli.sh
+++ docker/admin-cli.sh
@@ -1,4 +1,4 @@
 #To run this script, update the database information in the application.properties file. Check for the following properties in the application.properties file.
 #spring.datasource.url, spring.datasource.username, spring.datasource.password
 
-docker run -it --volume ./application.properties:/app/config/application.properties  admin-cli:dev
+docker run -it --volume ./application.properties:/app/config/application.properties  admin-cli:${tag}
