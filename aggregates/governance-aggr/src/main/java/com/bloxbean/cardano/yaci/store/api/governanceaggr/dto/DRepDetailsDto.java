e52477e53e18a4fdb01c39f079d912fb98329f80
Sotatek-HuyLe3a <144794833+Sotatek-HuyLe3a@users.noreply.github.com>
2025-04-21 17:24:16 +0700

    fix: Fix null fields in Dreps endpoint (#531)

--- aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/api/governanceaggr/dto/DRepDetailsDto.java
+++ aggregates/governance-aggr/src/main/java/com/bloxbean/cardano/yaci/store/api/governanceaggr/dto/DRepDetailsDto.java
@@ -1,6 +1,9 @@
 package com.bloxbean.cardano.yaci.store.api.governanceaggr.dto;
 
 import com.bloxbean.cardano.client.transaction.spec.governance.DRepType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -10,6 +13,8 @@ import java.math.BigInteger;
 @Getter
 @AllArgsConstructor
 @EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class DRepDetailsDto {
     private String drepId;
     private String drepHash;
