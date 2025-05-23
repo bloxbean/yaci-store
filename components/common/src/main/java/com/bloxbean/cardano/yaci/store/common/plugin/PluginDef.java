33555603d9ab2616d4b4481db9e69a139e3e777f
Satya <satran004@gmail.com>
2025-05-23 23:24:02 +0800

    Add PluginDef class to define plugin metadata structure

---
+++ components/common/src/main/java/com/bloxbean/cardano/yaci/store/common/plugin/PluginDef.java
@@ -0,0 +1,24 @@
+package com.bloxbean.cardano.yaci.store.common.plugin;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PluginDef {
+    private String name;
+    private String type;
+    private String expression; //nullable
+    private String inlineScript; //nullable
+    private Script script; //nullable
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Script {
+        private String file;
+        private String function; //nullable
+    }
+}
