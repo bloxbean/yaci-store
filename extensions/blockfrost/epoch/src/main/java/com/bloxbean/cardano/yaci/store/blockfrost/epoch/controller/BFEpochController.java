1a454232f8c60232250ac9d5493c6f0970b49004
ducpm2303 <minhducphung.it@gmail.com>
2026-02-05 10:53:17 +0700

    refactor: rename BFEpochsController to BFEpochController for consistency

--- extensions/blockfrost/epoch/src/main/java/com/bloxbean/cardano/yaci/store/blockfrost/epoch/controller/BFEpochController.java
+++ extensions/blockfrost/epoch/src/main/java/com/bloxbean/cardano/yaci/store/blockfrost/epoch/controller/BFEpochController.java
@@ -32,7 +32,7 @@ import java.util.List;
 @Tag(name = "Blockfrost Epochs")
 @RequestMapping("${blockfrost.apiPrefix}/epochs")
 @ConditionalOnExpression("${store.extensions.blockfrost.epoch.enabled:true}")
-public class BFEpochsController {
+public class BFEpochController {
 
     private final BFProtocolParamMapper bfProtocolParamMapper = BFProtocolParamMapper.INSTANCE;
     private final EpochParamService epochParamService;
