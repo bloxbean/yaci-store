20e9b8ffc9e8fec84bb6d379dcbb88fad8f0cca8
ducpm2303 <minhducphung.it@gmail.com>
2026-02-12 10:33:12 +0700

    fix: disable Blockfrost modules by default

--- extensions/blockfrost/epoch/src/main/java/com/bloxbean/cardano/yaci/store/blockfrost/epoch/BFEpochConfiguration.java
+++ extensions/blockfrost/epoch/src/main/java/com/bloxbean/cardano/yaci/store/blockfrost/epoch/BFEpochConfiguration.java
@@ -1,5 +1,7 @@
 package com.bloxbean.cardano.yaci.store.blockfrost.epoch;
 
+import com.bloxbean.cardano.yaci.store.api.epochaggr.service.EpochReadService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +9,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConditionalOnProperty(name = {"store.extensions.blockfrost.epoch.enabled"},
         havingValue = "true",
-        matchIfMissing = true
+        matchIfMissing = false
 )
+@ConditionalOnBean(EpochReadService.class)
 @ComponentScan(basePackages = {
         "com.bloxbean.cardano.yaci.store.blockfrost.epoch",
 })
