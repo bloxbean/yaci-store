e68e930658eba77bac80018480c8b86909417ed1
ducpm2303 <minhducphung.it@gmail.com>
2026-02-12 15:16:09 +0700

    chore: remove unnecessary ConditionalOnBean

--- extensions/blockfrost/epoch/src/main/java/com/bloxbean/cardano/yaci/store/blockfrost/epoch/BFEpochConfiguration.java
+++ extensions/blockfrost/epoch/src/main/java/com/bloxbean/cardano/yaci/store/blockfrost/epoch/BFEpochConfiguration.java
@@ -1,7 +1,5 @@
 package com.bloxbean.cardano.yaci.store.blockfrost.epoch;
 
-import com.bloxbean.cardano.yaci.store.api.epochaggr.service.EpochReadService;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
         havingValue = "true",
         matchIfMissing = false
 )
-@ConditionalOnBean(EpochReadService.class)
 @ComponentScan(basePackages = {
         "com.bloxbean.cardano.yaci.store.blockfrost.epoch",
 })
