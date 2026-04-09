f590139c0baa4836e73ed95a11a4b895fa1285f6
ducpm2303 <minhducphung.it@gmail.com>
2026-04-09 15:53:35 +0700

    fix: serialize reused Amount quantities as strings

--- extensions/blockfrost/account/src/main/java/com/bloxbean/cardano/yaci/store/blockfrost/account/dto/BFAccountAddressesTotalDto.java
+++ extensions/blockfrost/account/src/main/java/com/bloxbean/cardano/yaci/store/blockfrost/account/dto/BFAccountAddressesTotalDto.java
@@ -1,9 +1,11 @@
 package com.bloxbean.cardano.yaci.store.blockfrost.account.dto;
 
+import com.bloxbean.cardano.yaci.store.blockfrost.account.serializer.AmountAsStringSerializer;
 import com.bloxbean.cardano.yaci.store.utxo.domain.Amount;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,7 +21,9 @@ import java.util.List;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BFAccountAddressesTotalDto {
     private String stakeAddress;
+    @JsonSerialize(contentUsing = AmountAsStringSerializer.class)
     private List<Amount> receivedSum;
+    @JsonSerialize(contentUsing = AmountAsStringSerializer.class)
     private List<Amount> sentSum;
     private long txCount;
 }
