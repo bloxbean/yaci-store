075ff88c7a8342569a6522da5b6a8cdece034604
Satya <satran004@gmail.com>
2026-07-20 14:56:55 +0800

    feat: expose network identity and sync status

--- components/common/src/main/java/com/bloxbean/cardano/yaci/store/common/domain/SyncStatus.java
+++ components/common/src/main/java/com/bloxbean/cardano/yaci/store/common/domain/SyncStatus.java
@@ -1,5 +1,6 @@
 package com.bloxbean.cardano.yaci.store.common.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 @Builder
@@ -15,4 +16,10 @@ public record SyncStatus(
         boolean synced,
         long protocolMagic
 ) {
+    public static final long UNKNOWN_NETWORK_TIP = -1L;
+
+    @JsonProperty
+    public boolean networkTipAvailable() {
+        return networkBlock != UNKNOWN_NETWORK_TIP && networkSlot != UNKNOWN_NETWORK_TIP;
+    }
 }
